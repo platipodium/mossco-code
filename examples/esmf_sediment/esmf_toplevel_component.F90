326ce430b7c3fc3ebe837f1d1de04227d2fb0a68
Carsten Lemmen <carsten.lemmen@hzg.de>
2013-06-11 18:04:31 +0200

    Added esmf componenet example sources

---
+++ examples/esmf_sediment/esmf_toplevel_component.F90
@@ -0,0 +1,113 @@
+module esmf_toplevel_component
+
+  use esmf
+
+  ! Registration routines for fabm
+  use esmf_fabm_sediment_component, only : empty_SetServices
+
+  implicit none
+
+  private
+
+  public SetServices
+
+  type(ESMF_GridComp),save  :: fabmComp
+  type(ESMF_State),save     :: fabmExp, fabmImp
+
+  contains
+
+  subroutine SetServices(gridcomp, rc)
+
+    type(ESMF_GridComp)  :: gridcomp
+    integer, intent(out) :: rc
+
+    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, Initialize, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, Run, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_FINALIZE, Finalize, rc=rc)
+
+  end subroutine SetServices
+
+  subroutine Initialize(gridComp, importState, exportState, parentClock, rc)
+    
+    type(ESMF_GridComp)   :: gridComp
+    type(ESMF_State)      :: importState
+    type(ESMF_State)      :: exportState
+    type(ESMF_Clock)      :: parentClock
+    integer, intent(out)  :: rc
+
+    type(ESMF_Grid)       :: grid
+    type(ESMF_Field)      :: temperatureField
+    type(ESMF_FieldBundle) :: fieldBundle
+    integer               :: petCount, localPet
+
+    call ESMF_LogWrite("Toplevel component initializing ... ",ESMF_LOGMSG_INFO)
+    call ESMF_GridCompGet(gridComp,petCount=petCount,localPet=localPet)
+
+    ! Create a grid and a field (still to clarify how to access the array
+    grid = ESMF_GridCreateNoPeriDim(minIndex=(/1,1,1/), maxIndex=(/10,20,15/), &
+      regDecomp=(/2,2,1/), name="fabmGrid", rc=rc)
+    temperatureField = ESMF_FieldCreate(grid,typekind=ESMF_TYPEKIND_R8, &
+      indexflag=ESMF_INDEX_DELOCAL, staggerloc=ESMF_STAGGERLOC_CENTER, name = "temperature", rc=rc)
+    fieldBundle = ESMF_FieldBundleCreate(name="FABM field bundle", rc=rc)
+    call ESMF_FieldBundleAdd(fieldBundle, (/temperatureField/),rc=rc)
+
+
+    ! Create component, call setservices, and create states
+    fabmComp = ESMF_GridCompCreate(name="fabmComp", grid=grid, rc=rc)
+    call ESMF_GridCompSetServices(fabmComp,empty_SetServices, rc=rc)
+    
+    fabmImp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT,name="fabmImp")
+    fabmExp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT,name="fabmExp")
+
+    call ESMF_StateAdd(fabmImp, (/fieldBundle/), rc=rc)
+    call ESMF_StateAdd(fabmExp, (/fieldBundle/), rc=rc)
+
+    call ESMF_GridCompInitialize(fabmComp, importState=fabmImp, exportState=fabmExp, clock=parentClock, rc=rc)
+
+    call ESMF_LogWrite("Toplevel component initialized",ESMF_LOGMSG_INFO) 
+
+  end subroutine Initialize
+
+  subroutine Run(gridComp, importState, exportState, parentClock, rc)
+    
+    type(ESMF_GridComp)   :: gridComp
+    type(ESMF_State)      :: importState, exportState
+    type(ESMF_Clock)      :: parentClock
+    integer, intent(out)  :: rc
+
+    call ESMF_LogWrite("Toplevel component running ... ",ESMF_LOGMSG_INFO)
+
+    do while (.not. ESMF_ClockIsStopTime(parentClock, rc=rc))
+
+      call ESMF_ClockAdvance(parentClock, rc=rc)
+      if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+      
+      call ESMF_GridCompRun(fabmComp, importState=fabmImp, exportState=fabmExp, clock=parentClock, rc=rc)
+
+    enddo 
+
+    call ESMF_LogWrite("Toplevel component finished running. ",ESMF_LOGMSG_INFO)
+ 
+  end subroutine Run
+
+  subroutine Finalize(gridComp, importState, exportState, parentClock, rc)
+    
+    type(ESMF_GridComp)   :: gridComp
+    type(ESMF_State)      :: importState, exportState
+    type(ESMF_Clock)      :: parentClock
+    integer, intent(out)  :: rc
+
+    call ESMF_LogWrite("Toplevel component finalizing",ESMF_LOGMSG_INFO)
+
+    call ESMF_GridCompFinalize(fabmComp, importState=fabmImp, exportState=fabmExp, &
+      clock=parentClock, rc=rc)
+
+    call ESMF_GridCompDestroy(fabmComp, rc=rc)
+
+    call ESMF_LogWrite("Toplevel component finalized",ESMF_LOGMSG_INFO)
+   
+    rc=ESMF_SUCCESS
+
+  end subroutine Finalize
+
+end module esmf_toplevel_component
