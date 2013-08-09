164c128b949299e45901c805888ba39e2adac287
Carsten Lemmen <carsten.lemmen@hzg.de>
2013-08-09 13:09:54 +0200

    Added ESMF/GOTM driver

---
+++ src/components/esmf_gotm_component.F90
@@ -0,0 +1,75 @@
+!> @file esmf_gotm_component.F90
+!! @brief ESMF/GOTM driver component
+!!
+!! The ESMF component contains the gotm driver module
+!! @author Carsten Lemmen
+
+!> The ESMF/GOTM driver component module provides infrastructure for the
+!! MOSSCO GOTM component.
+
+module esmf_gotm_component
+
+  use esmf
+  use time
+  use gotm
+
+  implicit none
+
+  private
+ 
+  public :: empty_SetServices
+  
+  contains
+
+  !> Provide an ESMF compliant SetServices routine, which defines
+  !! the entry points for Init/Run/Finalize
+
+  subroutine empty_SetServices(gridcomp, rc)
+  
+    type(ESMF_GridComp)  :: gridcomp
+    integer, intent(out) :: rc
+
+    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, Initialize, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, Run, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_FINALIZE, Finalize, rc=rc)
+
+  end subroutine empty_SetServices
+
+  !> Initialize the component
+  !!
+  !! Allocate memory for boundaries and fluxes, create ESMF fields
+  !! and export them
+  subroutine Initialize(gridComp, importState, exportState, parentClock, rc)
+    implicit none
+
+    type(ESMF_GridComp)  :: gridComp
+    type(ESMF_State)     :: importState, exportState
+    type(ESMF_Clock)     :: parentClock
+    integer, intent(out) :: rc
+
+    call ESMF_LogWrite('Initialise GOTM',ESMF_LOGMSG_INFO)
+    call init_gotm()
+
+  end subroutine Initialize
+
+  subroutine Run(gridComp, importState, exportState, parentClock, rc)
+    type(ESMF_GridComp)  :: gridComp
+    type(ESMF_State)     :: importState, exportState
+    type(ESMF_Clock)     :: parentClock
+    integer, intent(out) :: rc
+ 
+    call time_loop()
+
+  end subroutine Run
+
+  subroutine Finalize(gridComp, importState, exportState, parentClock, rc)
+    type(ESMF_GridComp)  :: gridComp
+    type(ESMF_State)     :: importState, exportState
+    type(ESMF_Clock)     :: parentClock
+    integer, intent(out) :: rc
+
+    call clean_up()
+
+  end subroutine Finalize
+
+end module esmf_gotm_component
