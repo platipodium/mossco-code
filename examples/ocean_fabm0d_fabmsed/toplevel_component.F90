031f3da89557ce6013338dbf3271861192202d9c
Carsten Lemmen <carsten.lemmen@hzg.de>
2013-09-06 12:50:16 +0200

    Consistent capitalization, removed extra name variables

--- examples/ocean_fabm0d_fabmsed/toplevel_component.F90
+++ examples/ocean_fabm0d_fabmsed/toplevel_component.F90
@@ -13,30 +13,26 @@ module toplevel_component
   public SetServices
 
   type(ESMF_GridComp),save    :: fabm0dComp
-  character(len=ESMF_MAXSTR)  :: fabm0dCompName 
   type(ESMF_State)            :: fabm0dImportState, fabm0dExportState
 
   type(ESMF_GridComp),save    :: sedimentComp
-  character(len=ESMF_MAXSTR)  :: sedimentCompName 
   type(ESMF_State)            :: sedimentImportState, sedimentExportState
 
   type(ESMF_GridComp),save    :: oceanComp
-  character(len=ESMF_MAXSTR)  :: oceanCompName 
   type(ESMF_State)            :: oceanImportState, oceanExportState
 
   type(ESMF_CplComp),save     :: oscplComp
-  character(len=ESMF_MAXSTR)  :: oscplCompName 
 
   contains
 
   subroutine SetServices(gridcomp, rc)
 
-    type(ESMF_GridComp)  :: gridcomp
+    type(ESMF_GridComp)  :: gridComp
     integer, intent(out) :: rc
 
-    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, Initialize, rc=rc)
-    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, Run, rc=rc)
-    call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_FINALIZE, Finalize, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridComp, ESMF_METHOD_INITIALIZE, Initialize, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridComp, ESMF_METHOD_RUN, Run, rc=rc)
+    call ESMF_GridCompSetEntryPoint(gridComp, ESMF_METHOD_FINALIZE, Finalize, rc=rc)
 
   end subroutine SetServices
 
@@ -52,25 +48,23 @@ module toplevel_component
     
     call ESMF_LogWrite("Toplevel component initializing ... ",ESMF_LOGMSG_INFO)
     
-    sedimentCompName = "ESMF/FABM sediment component"
-    sedimentComp     = ESMF_GridCompCreate(name=sedimentCompName, contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
-    call ESMF_GridCompSetServices(sedimentcomp, sediment_SetServices, rc=rc)
+    sedimentComp     = ESMF_GridCompCreate(name="ESMF/FABM sediment component", &          
+                         contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
+    call ESMF_GridCompSetServices(sedimentComp, sediment_SetServices, rc=rc)
     sedimentImportState = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT,name="Sediment Import")
     sedimentExportState = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT,name="Sediment Export")
     call ESMF_GridCompInitialize(sedimentComp,importState=sedimentImportState,exportState=sedimentExportState,&
       clock=parentClock,rc=rc)
 
-    oceanCompName = "ESMF Remtc Ocean component"
-    oceanComp     = ESMF_GridCompCreate(name=oceanCompName, contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
-    call ESMF_GridCompSetServices(oceancomp, ocean_SetServices, rc=rc)
+    oceanComp     = ESMF_GridCompCreate(name="ESMF Ocean component", contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
+    call ESMF_GridCompSetServices(oceanComp, ocean_SetServices, rc=rc)
     oceanImportState = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT,name="Ocean Import")
     oceanExportState = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT,name="Ocean Export")
     call ESMF_GridCompInitialize(oceanComp,importState=oceanImportState,exportState=oceanExportState,&
       clock=parentClock,rc=rc)
 
-    oscplCompName = "ESMF/FABM ocean-sediment coupler component"
-    oscplComp     = ESMF_CplCompCreate(name=oscplCompName, contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
-    call ESMF_CplCompSetServices(oscplcomp, oscpl_SetServices, rc=rc)
+    oscplComp     = ESMF_CplCompCreate(name="O-S coupler component", contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
+    call ESMF_CplCompSetServices(oscplComp, oscpl_SetServices, rc=rc)
     call ESMF_CplCompInitialize(oscplComp,importState=sedimentExportState,exportState=oceanImportState,&
       clock=parentClock,rc=rc)
 
@@ -86,8 +80,8 @@ module toplevel_component
     integer, intent(out)  :: rc
 
     integer  :: myrank
-    type(ESMF_Time)             :: localtime
-    character (len=ESMF_MAXSTR) :: timestring,message
+    type(ESMF_Time)             :: localTime
+    character (len=ESMF_MAXSTR) :: timeString,message
 
     call ESMF_LogWrite("Toplevel component running ... ",ESMF_LOGMSG_INFO)
     call ESMF_GridCompGet(gridComp, localPet=myrank, rc=rc)
@@ -96,8 +90,8 @@ module toplevel_component
     do while (.not. ESMF_ClockIsStopTime(parentClock, rc=rc))
       call ESMF_ClockAdvance(parentClock, rc=rc)
       call ESMF_ClockGet(parentClock, currtime=localtime, rc=rc)
-      call ESMF_TimeGet(localtime, timeString=timestring, rc=rc)
-      message = "Toplevel ticking at "//trim(timestring)
+      call ESMF_TimeGet(localTime, timeString=timeString, rc=rc)
+      message = "Toplevel ticking at "//trim(timeString)
       call ESMF_LogWrite(message, ESMF_LOGMSG_INFO)
       call ESMF_GridCompRun(sedimentComp,importState=sedimentImportState,&
         exportState=sedimentExportState,clock=parentclock, rc=rc)
