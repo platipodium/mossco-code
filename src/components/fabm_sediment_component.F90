7db20295e573eab2111dbb6f6de5bd18af9a51f1
Richard <richard.hofmeister@hzg.de>
2014-02-11 21:12:27 +0100

    added mossco_strings

--- src/components/fabm_sediment_component.F90
+++ src/components/fabm_sediment_component.F90
@@ -31,6 +31,7 @@ module fabm_sediment_component
   use fabm
   use fabm_sediment_driver
   use solver_library!, only : ode_solver
+  use mossco_strings
 
   implicit none
 
@@ -441,39 +442,5 @@ module fabm_sediment_component
     end do
 
   end subroutine get_boundary_conditions
-
-   function only_var_name(longname)
-      character(:),allocatable     :: only_var_name
-      character(len=*), intent(in) :: longname
-      character(len=256)           :: words(100)
-      integer                      :: pos,pos1=1,pos2,i
-
-      !> remove model name
-      pos = INDEX(longname, " ")
-      allocate(character(len=len_trim(longname)-pos)::only_var_name)
-      only_var_name = trim(longname(pos+1:))
-
-      pos1=1
-      pos =0
-      !> replace white space with underscore
-      do
-         pos2 = INDEX(only_var_name(pos1:), " ")
-         IF (pos2 == 0) THEN
-            pos = pos + 1
-            words(pos) = only_var_name(pos1:)
-            EXIT
-         END IF
-         pos = pos + 1
-         words(pos) = only_var_name(pos1:pos1+pos2-2)
-         pos1 = pos2+pos1
-      end do
- 
-      only_var_name=trim(words(1))
-      do i = 2, pos
-         only_var_name=trim(only_var_name)//"_"//trim(words(i))
-      end do
-
-   end function only_var_name
-
 
 end module fabm_sediment_component
--- src/drivers/fabm_gotm_driver.F90
+++ src/drivers/fabm_gotm_driver.F90
@@ -20,6 +20,7 @@ use fabm
 use fabm_types
 use fabm_config
 use solver_library
+use mossco_strings
 
 implicit none
 
@@ -907,38 +908,5 @@ end type
 
       write (*,*) trim(message)
    end subroutine
-
-   function only_var_name(longname)
-      character(:),allocatable     :: only_var_name
-      character(len=*), intent(in) :: longname
-      character(len=256)           :: words(100)
-      integer                      :: pos,pos1=1,pos2,i
-
-      !> remove model name
-      pos = INDEX(longname, " ")
-      allocate(character(len=len_trim(longname)-pos)::only_var_name)
-      only_var_name = trim(longname(pos+1:))
-
-      pos1=1
-      pos =0
-      !> replace white space with underscore
-      do
-         pos2 = INDEX(only_var_name(pos1:), " ")
-         IF (pos2 == 0) THEN
-            pos = pos + 1
-            words(pos) = only_var_name(pos1:)
-            EXIT
-         END IF
-         pos = pos + 1
-         words(pos) = only_var_name(pos1:pos1+pos2-2)
-         pos1 = pos2+pos1
-      end do
- 
-      only_var_name=trim(words(1))
-      do i = 2, pos
-         only_var_name=trim(only_var_name)//"_"//trim(words(i))
-      end do
-
-   end function only_var_name
 
 end module
