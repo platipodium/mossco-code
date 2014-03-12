81fd8c3f57a569406a7b24eaf8cfab098b3b8d39
Carsten Lemmen <carsten.lemmen@hzg.de>
2014-03-12 19:22:54 +0100

    Fixed netcdf put routine for 1d, 2d, 3d fields

--- src/utilities/mossco_netcdf.F90
+++ src/utilities/mossco_netcdf.F90
@@ -103,10 +103,17 @@ module mossco_netcdf
     if (ncStatus /= NF90_NOERR) call ESMF_LogWrite(nf90_strerror(ncStatus),ESMF_LOGMSG_ERROR)
     
     if (rank==3) then
-      ncStatus = nf90_put_var(self%ncid, varid, farrayPtr3, start=(/1,1,1,dimlen/))
+      call  ESMF_FieldGet(field, farrayPtr=farrayPtr3, rc=rc)
+      if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT) 
+      ncStatus = nf90_put_var(self%ncid, varid, farrayPtr3, &
+        start=(/1,1,1,dimlen/), count=(/1,1,1,1/))
     elseif (rank==2) then
+      call  ESMF_FieldGet(field, farrayPtr=farrayPtr2, rc=rc)
+      if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT) 
       ncStatus = nf90_put_var(self%ncid, varid, farrayPtr3, start=(/1,1,dimlen/))
     elseif (rank==2) then
+      call  ESMF_FieldGet(field, farrayPtr=farrayPtr1, rc=rc)
+      if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT) 
       ncStatus = nf90_put_var(self%ncid, varid, farrayPtr3, start=(/1,dimlen/))
     endif
     if (ncStatus /= NF90_NOERR) call ESMF_LogWrite(nf90_strerror(ncStatus),ESMF_LOGMSG_ERROR)
