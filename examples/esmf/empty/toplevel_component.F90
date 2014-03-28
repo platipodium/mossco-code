6939a3d3eb24f5fd12df51ac5a805b42f26a1f07
Carsten Lemmen <carsten.lemmen@hzg.de>
2014-03-28 21:31:22 +0100

    Actually run empty in its own example :=)

--- examples/esmf/empty/toplevel_component.F90
+++ examples/esmf/empty/toplevel_component.F90
@@ -1,12 +1,17 @@
 module toplevel_component
 
   use esmf
+  use empty_component, only: empty_SetServices => SetServices
 
   implicit none
 
   private
 
   public SetServices
+
+  type(ESMF_GridComp),save    :: emptyComp
+  character(len=ESMF_MAXSTR)  :: emptyCompName
+  type(ESMF_State)            :: emptyImportState, emptyExportState
 
   contains
 
@@ -22,64 +27,75 @@ module toplevel_component
   end subroutine SetServices
 
   subroutine Initialize(gridComp, importState, exportState, parentClock, rc)
-    
+
     type(ESMF_GridComp)   :: gridComp
     type(ESMF_State)      :: importState
     type(ESMF_State)      :: exportState
     type(ESMF_Clock)      :: parentClock
     integer, intent(out)  :: rc
 
-    type(ESMF_Grid)       :: grid
-    type(ESMF_Field)      :: temperatureField
-    type(ESMF_FieldBundle) :: fieldBundle
     integer               :: petCount, localPet
 
     call ESMF_LogWrite("Toplevel component initializing ... ",ESMF_LOGMSG_INFO)
-    call ESMF_GridCompGet(gridComp,petCount=petCount,localPet=localPet)
 
-    ! Create a grid and a field (still to clarify how to access the array
-    grid = ESMF_GridCreateNoPeriDim(minIndex=(/1,1,1/), maxIndex=(/10,20,15/), &
-      regDecomp=(/2,2,1/), name="fabmGrid", rc=rc)
-    temperatureField = ESMF_FieldCreate(grid,typekind=ESMF_TYPEKIND_R8, &
-      indexflag=ESMF_INDEX_DELOCAL, staggerloc=ESMF_STAGGERLOC_CENTER, name = "temperature", rc=rc)
-    fieldBundle = ESMF_FieldBundleCreate(name="FABM field bundle", rc=rc)
-    call ESMF_FieldBundleAdd(fieldBundle, (/temperatureField/),rc=rc)
+    emptyCompName = "ESMF empty component"
+    emptyComp     = ESMF_GridCompCreate(name=emptyCompName, contextflag=ESMF_CONTEXT_PARENT_VM,rc=rc)
+    call ESMF_GridCompSetServices(emptycomp, empty_SetServices, rc=rc)
+    emptyImportState = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT,name="empty Import")
+    emptyExportState = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_EXPORT,name="empty Export")
+    call ESMF_GridCompInitialize(emptyComp,importState=emptyImportState,exportState=emptyExportState,&
+      clock=parentClock,rc=rc)
 
-    call ESMF_LogWrite("Toplevel component initialized",ESMF_LOGMSG_INFO) 
+    call ESMF_LogWrite("Toplevel component initialized",ESMF_LOGMSG_INFO)
 
   end subroutine Initialize
 
   subroutine Run(gridComp, importState, exportState, parentClock, rc)
-    
+
     type(ESMF_GridComp)   :: gridComp
     type(ESMF_State)      :: importState, exportState
     type(ESMF_Clock)      :: parentClock
     integer, intent(out)  :: rc
 
+    integer  :: myrank
+    type(ESMF_Time)             :: localtime
+    character (len=ESMF_MAXSTR) :: timestring,message
+
     call ESMF_LogWrite("Toplevel component running ... ",ESMF_LOGMSG_INFO)
+    call ESMF_GridCompGet(gridComp, localPet=myrank, rc=rc)
+    if(rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
 
     do while (.not. ESMF_ClockIsStopTime(parentClock, rc=rc))
+      call ESMF_ClockGet(parentClock, currtime=localtime, rc=rc)
+      call ESMF_TimeGet(localtime, timeString=timestring, rc=rc)
+      message = "Toplevel ticking at "//trim(timestring)
+      call ESMF_LogWrite(message, ESMF_LOGMSG_INFO)
+
+      call ESMF_GridCompRun(emptyComp,importState=emptyImportState,&
+        exportState=emptyExportState,clock=parentclock, rc=rc)
+      if(rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
 
       call ESMF_ClockAdvance(parentClock, rc=rc)
-      if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-      
-    enddo 
+      if(rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
+   enddo
 
     call ESMF_LogWrite("Toplevel component finished running. ",ESMF_LOGMSG_INFO)
- 
+
   end subroutine Run
 
   subroutine Finalize(gridComp, importState, exportState, parentClock, rc)
-    
+
     type(ESMF_GridComp)   :: gridComp
     type(ESMF_State)      :: importState, exportState
     type(ESMF_Clock)      :: parentClock
     integer, intent(out)  :: rc
 
-    call ESMF_LogWrite("Toplevel component finalizing",ESMF_LOGMSG_INFO)
+    call ESMF_GridCompFinalize(emptyComp,importState=emptyImportState,exportState=emptyExportState, &
+                            clock=parentclock, rc=rc)
+    call ESMF_GridCompDestroy(emptyComp,rc=rc)
 
     call ESMF_LogWrite("Toplevel component finalized",ESMF_LOGMSG_INFO)
-   
+
     rc=ESMF_SUCCESS
 
   end subroutine Finalize
