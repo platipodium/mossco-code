3d634dd0b168a496c7344f9495cf91ace87f32d0
Knut <knutaros@gmx.net>
2014-04-02 13:50:12 +0200

    added some LOGMSG_TRACE strings to esmf/getm/toplevel_component.F90

--- examples/esmf/getm/toplevel_component.F90
+++ examples/esmf/getm/toplevel_component.F90
@@ -43,6 +43,8 @@ module toplevel_component
     type(ESMF_TimeInterval) :: runDuration
     logical                 :: ClockIsPresent
 
+    call ESMF_LogWrite("Toplevel component initializing ... ",ESMF_LOGMSG_TRACE)
+
 !   Check whether application driver called ESMF_GridCompCreate() with clock.
     call ESMF_GridCompGet(topCmp,clockIsPresent=ClockIsPresent)
 
@@ -73,6 +75,8 @@ module toplevel_component
       end if
     end if
 
+    call ESMF_LogWrite("Toplevel component initialized",ESMF_LOGMSG_TRACE)
+
     rc = ESMF_SUCCESS
 
   end subroutine topCmp_init
@@ -89,6 +93,8 @@ module toplevel_component
     type(ESMF_Time)         :: topTime,NextTime
     type(ESMF_TimeInterval) :: topTimeStep
     integer                 :: localrc
+
+    call ESMF_LogWrite("Toplevel component running ... ",ESMF_LOGMSG_TRACE)
 
     call ESMF_ClockGet(topClock,timeStep=topTimeStep,currtime=topTime)
 
@@ -115,6 +121,8 @@ module toplevel_component
       call ESMF_ClockGet(topClock,currtime=topTime)
 
     end do
+
+    call ESMF_LogWrite("Toplevel component finished running. ",ESMF_LOGMSG_TRACE)
  
   end subroutine topCmp_run
 
@@ -127,6 +135,8 @@ module toplevel_component
     type(ESMF_Clock)    :: pClock
     integer,intent(out) :: rc
 
+    call ESMF_LogWrite("Toplevel component finalizing ... ",ESMF_LOGMSG_TRACE)
+
 !   Finalize of child components
     call ESMF_GridCompFinalize(getmCmp,clock=topClock)
 
@@ -134,6 +144,8 @@ module toplevel_component
     call ESMF_GridCompDestroy(getmCmp)
 
     call ESMF_ClockDestroy(topClock)
+
+    call ESMF_LogWrite("Toplevel component finalized",ESMF_LOGMSG_TRACE)
    
     rc=ESMF_SUCCESS
 
