c82303593d8f4ca4e4f068f298b8f122c7b086e1
Ulrich Koerner <koerneru@ocean-fe.fzg.local>
2014-04-22 18:23:43 +0200

    new script for getm parallel test on ocean

---
+++ examples/generic/test_getm_parallel_yaml.sh
@@ -0,0 +1,114 @@
+#! /bin/bash
+
+# @brief test routine for getm couplings
+#
+# This computer program is part of MOSSCO. 
+# @copyright Copyright 2014, Helmholtz-Zentrum Geesthacht
+# @author Carsten Lemmen <carsten.lemmen@hzg.de>
+
+#
+# MOSSCO is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License v3+.  MOSSCO is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY.  Consult the file
+# LICENSE.GPL or www.gnu.org/licenses/gpl-3.0.txt for the full license terms.
+#
+
+NPROCS=4
+if [ "x$1" == "x" ]; then
+  FILTER="*.yaml"
+else
+  FILTER="$1.yaml"
+fi
+FILTER=getm_coupling.yaml
+
+exampleSetupName=box_cartesian
+MOSSCO_SETUPDIR=${MOSSCO_DIR%code}setups
+S=${GETM_SETUPDIR}/${exampleSetupName}
+L=${0%.sh}.log
+G=$MOSSCO_DIR/examples/generic
+H=$(uname -mns)
+D=$(date "+%Y%m%d %H:%M:00")
+
+cat << EOT >> $L
+
+Running tests on host $H with filter $FILTER on $D
+
+Host system | configuration | date | Result
+------------|---------------|------|--------
+EOT
+
+
+for F in $G/${FILTER}; do
+  D=$(date "+%Y%m%d %H:%M:00")
+  P=$S/PET0.${exampleSetupName}
+  B=$(basename $F .yaml)
+
+  python $G/create_coupling.py $F
+
+  if [ $? -eq 0 ]; then :
+  else
+    echo "$H | $B | $D | python failed" >> $L
+    continue
+  fi
+ 
+  if [ $(hostname) == ocean-fe.fzg.local ] ; then
+    make -C $G all
+  else
+    make -j4 -C $G all
+  fi
+  if [ $? -eq 0 ]; then :
+  else
+    echo "$H | $B | $D | make failed" >> $L
+    continue
+  fi
+  
+  if [ $(hostname) == ocean-fe.fzg.local ] ; then
+cat << EOT > $S/job.sh
+#!/bin/sh
+
+#$ -N  $B
+#$ -pe orte $NPROCS
+#$ -cwd
+#$ -V
+
+cat $PE_HOSTFILE
+mpirun -np $NPROCS $G/coupling > $S/$B.log
+EOT
+    (cd $S; rm -f $P net*.nc; qsub $S/job.sh )
+    while ! [ -f $P ] ; do
+      sleep 1
+    done
+
+  else
+    (cd $S ; rm -f $P net*.nc ;  $G/coupling)
+    if [ $? -ne 0 ]; then
+      echo "$H | $B | $D | run failed" >> $L
+      continue
+    fi
+    if [ -f $P ]; then : 
+    else
+      echo "$H | $B | $D | failed, no PET.log $P" >> $L
+      continue
+    fi
+  fi
+  
+exit
+  
+  mv $P ${P%.log}_$B.log
+  P=${P%.log}_$B.log
+  
+  W=$(grep -c WARNING $P)
+  E=$(grep -c ERROR $P)
+  
+  if [ $E -ne 0 ]; then
+    echo "$H | $B | $D | failed, $E errors, $W warnings " >> $L  
+    continue
+  fi
+
+  echo "$H | $B | $D | succeeded, $W warnings " >> $L   
+  
+  if [ $W -eq 0 ] ; then
+    rm -f $P
+  fi
+  
+done
