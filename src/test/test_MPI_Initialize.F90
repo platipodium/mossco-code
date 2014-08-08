89a0c96036cc3931458ac15aecff0549e4903480
Carsten Lemmen <carsten.lemmen@hzg.de>
2014-08-08 11:31:32 +0200

    Expanded MPI tests

--- src/test/test_MPI_Initialize.F90
+++ src/test/test_MPI_Initialize.F90
@@ -5,25 +5,51 @@
 
 program test_MPI_Initialize
 
-#ifdef MOSSCO_MPI
 
 use mpi
 implicit none
 
-integer ierror, petCount, localPet, rc
+integer ierr, size, rank, rc 
+logical flag
 
-call MPI_Init(ierror)
-if (ierror /= MPI_SUCCESS) then
-  write(0,*) 'MPI could not be initialized'
-  call MPI_Abort(MPI_COMM_WORLD, rc, ierror)
+call MPI_Finalized(flag, ierr)
+if (flag) then
+  write(0,*) 'MPI finalized before initialized'
 endif
 
-call MPI_Comm_Size(MPI_COMM_WORLD, petCount, ierror)
-call MPI_Comm_Rank(MPI_COMM_WORLD, localPet, ierror)
+call MPI_Initialized(flag, ierr)
+if (flag) then
+  write(0,*) 'MPI initialized before initialized'
+endif
 
-write(*,'(A,I2,A,I2,A)') 'MPI running on PET ',localPet,' of total ',petCount,' PETs.'
-call MPI_Finalize(ierror)
+      call mpi_init_thread( MPI_THREAD_MULTIPLE, -1, ierr )
 
-#endif
+call MPI_Init(ierr)
+if (ierr /= MPI_SUCCESS) then
+  write(0,*) 'MPI could not be initialized'
+  call MPI_Abort(MPI_COMM_WORLD, rc, ierr)
+endif
+
+
+call MPI_Initialized(flag, ierr)
+if (.not.flag) then
+  write(0,*) 'MPI not initialized after initialize'
+endif
+
+
+call MPI_Comm_Size(MPI_COMM_WORLD, size, ierr)
+if (ierr /= MPI_SUCCESS) then
+  write(0,*) 'MPI could not be get COMM_SIZE'
+  call MPI_Abort(MPI_COMM_WORLD, rc, ierr)
+endif
+
+call MPI_Comm_Rank(MPI_COMM_WORLD, rank, ierr)
+if (ierr /= MPI_SUCCESS) then
+  write(0,*) 'MPI could not be get COMM_RANK'
+  call MPI_Abort(MPI_COMM_WORLD, rc, ierr)
+endif
+
+write(*,'(A,I2,A,I2,A)') 'MPI running on PET ',rank,' of total ',size,' PETs.'
+call MPI_Finalize(ierr)
 
 end program 
