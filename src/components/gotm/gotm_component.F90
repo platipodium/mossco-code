0cee5f558c25b0846f2083fc46ebcc880fa5d0c4
Richard <richard.hofmeister@hzg.de>
2014-09-09 21:40:07 +0200

    gotm_component: set clock timestep from gotmrun namelist

--- src/components/gotm/gotm_component.F90
+++ src/components/gotm/gotm_component.F90
@@ -164,6 +164,8 @@ module gotm_component
     ! copy start and stop time from clock to gotm's time parameters
     call ESMF_TimeIntervalSet(timeInterval,s_r8=gotm_time_timestep,rc=rc)
     if(rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
+    call ESMF_ClockSet(clock, timeStep=timeInterval, rc=rc)
+    if(rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
     
     call ESMF_TimeGet(currTime,timeStringISOFrac=timestring,rc=rc)
     if(rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
