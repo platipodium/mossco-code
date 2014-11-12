a93dcc76bbede9e62a5f20f181361f4e25a703bf
Knut <knutaros@gmx.net>
2014-11-12 22:18:34 +0100

    test: print [l|u]bounds for test_pointer

--- src/test/test_pointer.F90
+++ src/test/test_pointer.F90
@@ -7,6 +7,7 @@ values(:,:,:) = .false.
 write(0,*) 'original data modified:',values(6,2,3) 
 
 ptr => values(5:15,2:4,3:5)
+write(*,*) lbound(ptr),ubound(ptr)
 ptr(2,1,1) = .true.
 write(0,*) 'data modified with pointer:',values(6,2,3) 
 
