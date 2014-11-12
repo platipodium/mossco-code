466d4d73ca01f3f2b7de8c2eeb54a5494696c94b
Richard <richard.hofmeister@hzg.de>
2014-11-12 19:27:21 +0100

    added test_pointer.F90 for subset pointer usage

---
+++ src/test/test_pointer.F90
@@ -0,0 +1,13 @@
+program test_pointer
+
+logical, dimension(:,:,:), pointer :: values=>null(),ptr=>null()
+
+allocate(values(20,10,5))
+values(:,:,:) = .false.
+write(0,*) 'original data modified:',values(6,2,3) 
+
+ptr => values(5:15,2:4,3:5)
+ptr(2,1,1) = .true.
+write(0,*) 'data modified with pointer:',values(6,2,3) 
+
+end program
