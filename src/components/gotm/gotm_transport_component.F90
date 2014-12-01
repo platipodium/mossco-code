4dd1213701d57ef7117ead45cb46a8b119ed79e7
Richard <richard.hofmeister@hzg.de>
2014-12-01 17:49:24 +0100

    gotm_transport_comp: update search for variables to consistent naming and fixed memory access

--- src/components/gotm/gotm_transport_component.F90
+++ src/components/gotm/gotm_transport_component.F90
@@ -41,7 +41,6 @@ module gotm_transport_component
 
   !> local variables for the setup control
   character(len=80)         :: title,name
-  integer                   :: nlev
   GOTM_REALTYPE             :: cnpar,latitude,longitude,depth
   GOTM_REALTYPE             :: T0,S0,p0,dtr0,dsr0
   integer                   :: buoy_method,eq_state_mode,eq_state_method
@@ -75,6 +74,7 @@ module gotm_transport_component
   !! create list of pointers to the 1D fields in the importState;
   !! the exportState is not used
   subroutine Initialize(gridComp, importState, exportState, parentClock, rc)
+    use meanflow, only : gotm_heights => h 
     implicit none
 
     type(ESMF_GridComp)         :: gridComp
@@ -87,6 +87,7 @@ module gotm_transport_component
     real(ESMF_KIND_R8)          :: dt
     integer                     :: lbnd(3), ubnd(3),farray_shape(3)
     integer                     :: i,ii,namelen
+    integer                     :: nlev
     integer                     :: itemcount, fieldCount
     type(ESMF_StateItem_Flag), allocatable, dimension(:)  :: itemTypeList
     character(len=ESMF_MAXSTR), allocatable, dimension(:) :: itemNameList
@@ -148,25 +149,32 @@ module gotm_transport_component
           namelen=len_trim(varname)
           call ESMF_StateGet(importState, varname, concfield, rc=rc) 
           if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-        
-          if (varname((namelen-11):namelen)=='_z_velocity') cycle
-         call ESMF_StateGet(importState, trim(varname)//'_z_velocity', wsfield, rc=rc)
-          ! go to next item, if no *_z_velocity is present
-          if (rc /= ESMF_SUCCESS) cycle
 
-          call ESMF_FieldGet(concfield, farrayPtr=tracer_ptr%conc, rc=rc)
-          if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT) 
-          call ESMF_FieldGet(wsfield, farrayPtr=tracer_ptr%ws, rc=rc)
-          if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-          call tracer_append(tracer,tracer_ptr)
+          if (varname((namelen-8):namelen) == '_in_water') then
+           if (namelen>20) then
+            if (varname((namelen-19):(namelen-9))=='_z_velocity') cycle
+           end if
+            call ESMF_StateGet(importState, varname(1:namelen-9)//'_z_velocity_in_water', wsfield, rc=rc)
+            ! go to next item, if no *_z_velocity is present
+            if (rc /= ESMF_SUCCESS) cycle
+
+            call ESMF_FieldGet(concfield, farrayPtr=tracer_ptr%conc, rc=rc)
+            if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT) 
+            call ESMF_FieldGet(wsfield, farrayPtr=tracer_ptr%ws, rc=rc)
+            if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+            call tracer_append(tracer,tracer_ptr)
+          end if
 
 
         elseif (itemTypeList(i) == ESMF_STATEITEM_FIELDBUNDLE) then
           varname=trim(itemNameList(i))
           namelen=len_trim(varname)
 
-          if (varname((namelen-10):namelen)=='_z_velocity') cycle
-          call ESMF_StateGet(importState, trim(varname)//'_z_velocity', wsFieldBundle, rc=rc)
+          if (varname((namelen-8):namelen) == '_in_water') then
+           if (namelen>20) then
+            if (varname((namelen-19):(namelen-9))=='_z_velocity') cycle
+           end if
+          call ESMF_StateGet(importState, varname(1:namelen-9)//'_z_velocity_in_water', wsFieldBundle, rc=rc)
           ! go to next item, if no *_z_velocity is present
           if (rc /= ESMF_SUCCESS) cycle
 
@@ -191,12 +199,14 @@ module gotm_transport_component
           end do
           deallocate(fieldList)
           deallocate(wsFieldList)
+          end if
         end if
       end do
 
     end if ! itemcount>0
 
     !> create helper arrays
+    nlev = size(gotm_heights)-1
     allocate(ones(0:nlev))
     ones=_ONE_
     allocate(zeros(0:nlev))
@@ -260,6 +270,9 @@ module gotm_transport_component
     write(message,'(A)') trim(message)//' '//trim(timeString)//' ...'
     call ESMF_LogWrite(trim(message), ESMF_LOGMSG_TRACE)
 
+    ! get number of layers as 3rd dimension of tracer pointer
+    knum = ubound(zeros,1)
+
     ! @todo implement a solution for short outer timesteps or non-integer number of internal vs outer timesteps
      do while (.not.ESMF_ClockIsStopTime(clock))
 
@@ -269,10 +282,6 @@ module gotm_transport_component
        ! Vertical advection and residual movement (sinking/floating)
        do i=1,ubound(tracer,1)
 
-         ! get number of layers as 3rd dimension of tracer pointer
-         knum = ubound(tracer(i)%conc,3)
-
-write(0,*) 'ws',shape(tracer(i)%ws), 'conc', shape(tracer(i)%conc)
          ! Do advection step due to settling or rising
          call adv_center(knum,dt,gotm_heights, gotm_heights, &
            tracer(i)%ws(1,1,:), flux, flux, _ZERO_, _ZERO_, &
@@ -285,9 +294,9 @@ write(0,*) 'ws',shape(tracer(i)%ws), 'conc', shape(tracer(i)%conc)
              w_adv_ctr,0,tracer(i)%conc(1,1,:))
 
          ! Vertical diffusion
-         call diff_center(knum,dt,cnpar,1,gotm_heights, &
+         call diff_center(knum,dt,cnpar,0,gotm_heights, &
             Neumann, Neumann, _ZERO_, _ZERO_, &
-            diffusivity,zeros, zeros, relaxTau, &
+            diffusivity, zeros, zeros, relaxTau, &
             tracer(i)%conc(1,1,:),tracer(i)%conc(1,1,:))
        end do
 
@@ -331,229 +340,5 @@ write(0,*) 'ws',shape(tracer(i)%ws), 'conc', shape(tracer(i)%conc)
   tracer(n)=tracerPtr
   end subroutine
 
-
-#if 0
-   subroutine mossco_diff_center(N,dt,cnpar,h,nuY,Y)
-   use util,          only  : Dirichlet, Neumann
-   use mtridiagonal
-
-!  number of vertical layers
-   integer,  intent(in)                :: N
-
-!  time step (s)
-   GOTM_REALTYPE, intent(in)           :: dt
-
-!  "implicitness" parameter
-   GOTM_REALTYPE, intent(in)           :: cnpar
-
-!  layer thickness (m)
-   GOTM_REALTYPE, intent(in)           :: h(0:N)
-
-!  diffusivity of Y
-   GOTM_REALTYPE, intent(in)           :: nuY(0:N)
-
-! !INPUT/OUTPUT PARAMETERS:
-   GOTM_REALTYPE                       :: Y(1:N)
-
-!  Original author(s): Lars Umlauf
-   integer                        :: i
-   GOTM_REALTYPE                  :: YY(0:N)
-   GOTM_REALTYPE                  :: a,c,l
-
-   YY(1:N) = Y
-
-   !  set up matrix
-   do i=2,N-1
-      c     = 2.0*dt*nuY(i)  /(h(i)+h(i+1))/h(i)
-      a     = 2.0*dt*nuY(i-1)/(h(i)+h(i-1))/h(i)
-
-      cu(i) =-cnpar*c
-      au(i) =-cnpar*a
-      bu(i) = _ONE_ + cnpar*(a + c)
-      du(i) = (_ONE_ - (_ONE_-cnpar)*(a + c))*Y(i)                  &
-            + (_ONE_ - cnpar)*( a*Y(i-1) + c*Y(i+1) )
-   end do
-
-   ! upper boundary
-      a     = 2.0*dt*nuY(N-1)/(h(N)+h(N-1))/h(N)
-      au(N) =-cnpar*a
-      bu(N) =  _ONE_ - au(N)
-      du(N) = Y(N) + (_ONE_ - cnpar)*a*(Y(N-1)-Y(N))
-
-   ! lower boundary
-      c     = 2.0*dt*nuY(1)/(h(1)+h(2))/h(1)
-      cu(1) =-cnpar*c
-      bu(1) = _ONE_ - cu(1)
-      du(1) = Y(1) + (_ONE_ - cnpar)*c*(Y(2)-Y(1))
-
-!  solve linear system
-   call tridiagonal(N,1,N,YY)
-   Y=YY(1:N)
-   
-   return
-   end subroutine mossco_diff_center
-
-
-   subroutine mossco_adv_center(N,dt,h,ww,method,Y)
-      use util
-   IMPLICIT NONE
-!
-! !INPUT PARAMETERS:
-
-!  number of vertical layers
-   integer,  intent(in)                :: N
-
-!  time step (s)
-   GOTM_REALTYPE, intent(in)                :: dt
-
-!  layer thickness (m)
-   GOTM_REALTYPE, intent(in)                :: h(0:N)
-
-!  vertical advection speed
-   GOTM_REALTYPE, intent(in)                :: ww(1:N)
-
-   !  type of advection scheme
-   integer,  intent(in)                :: method
-!
-! !INPUT/OUTPUT PARAMETERS:
-   GOTM_REALTYPE                            :: Y(1:N)
-!
-! !DEFINED PARAMETERS:
-   GOTM_REALTYPE,     parameter             :: one6th=1.0d0/6.0d0
-   integer,      parameter             :: itmax=100
-!
-! !REVISION HISTORY:
-!  Original author(s): Lars Umlauf
-!
-!EOP
-!
-! !LOCAL VARIABLES:
-   integer                              :: i,k,it
-   GOTM_REALTYPE                             :: x,r,Phi,limit
-   GOTM_REALTYPE                             :: Yu,Yc,Yd
-   GOTM_REALTYPE                             :: c,cmax
-   GOTM_REALTYPE                             :: cu(0:N)
-!
-!-----------------------------------------------------------------------
-!BOC
-
-!  initialize interface fluxes with zero
-   cu   = _ZERO_
-
-!  initialize maximum Courant number
-   cmax = _ZERO_
-
-!  compute maximum Courant number
-   do k=1,N-1
-      c=abs(ww(k))*dt/(0.5*(h(k)+h(k+1)))
-      if (c.gt.cmax) cmax=c
-   enddo
-
-   it=min(itmax,int(cmax)+1)
-
-#define STDERR write(0,*)
-!#ifdef DEBUG
-   if (it .gt. 1) then
-      STDERR 'In adv_center():'
-      STDERR 'Maximum Courant number is ',cmax
-      STDERR it,' iterations used for vertical advection'
-   endif
-!#endif
-
-!  splitting loop
-   do i=1,it
-
-!     vertical loop
-      do k=1,N-1
-
-!        compute the slope ration
-         if (ww(k) .gt. _ZERO_) then
-
-!           compute Courant number
-            c=ww(k)/float(it)*dt/(0.5*(h(k)+h(k+1)))
-
-            if (k .gt. 1) then
-               Yu=Y(k-1)                              ! upstream value
-            else
-               Yu=Y(k)
-            end if
-            Yc=Y(k  )                                 ! central value
-            Yd=Y(k+1)                                 ! downstream value
-
-!           compute slope ration
-            if (abs(Yd-Yc) .gt. 1e-10) then
-               r=(Yc-Yu)/(Yd-Yc)
-            else
-               r=(Yc-Yu)*1.e10
-            end if
-
-!        negative speed
-         else
-
-!           compute Courant number
-            c=-ww(k)/float(it)*dt/(0.5*(h(k)+h(k+1)))
-
-            if (k .lt. N-1) then
-               Yu=Y(k+2)                              ! upstream value
-            else
-               Yu=Y(k+1)
-            end if
-            Yc=Y(k+1)                                 ! central value
-            Yd=Y(k  )                                 ! downstream value
-
-
-!           compute slope ratio
-            if (abs(Yc-Yd) .gt. 1e-10) then
-               r=(Yu-Yc)/(Yc-Yd)
-            else
-               r=(Yu-Yc)*1.e10
-            end if
-
-         end if
-
-!        compute the flux-factor phi
-         x    =  one6th*(1.-2.0*c)
-         Phi  =  (0.5+x)+(0.5-x)*r
-
-!        limit the flux according to different suggestions
-         select case (method)
-            case (UPSTREAM)
-               limit=_ZERO_
-            case (P1)
-               STDERR "P1 advection method not yet implemented, choose other method"
-               stop  "adv_center.F90"
-            case ((P2),(P2_PDM))
-               if (method.eq.P2) then
-                  limit=Phi
-               else
-                  limit=max(_ZERO_,min(Phi,2./(1.-c),2.*r/(c+1.e-10)))
-               end if
-            case (Superbee)
-               limit=max(_ZERO_, min(_ONE_, 2.0*r), min(r,2.*_ONE_) )
-            case (MUSCL)
-               limit=max(_ZERO_,min(2.*_ONE_,2.0*r,0.5*(1.0+r)))
-            case default
-               STDERR method
-               STDERR 'unkown advection method in adv_center()'
-               stop
-          end select
-
-!        compute the limited flux
-         cu(k)=ww(k)*(Yc+0.5*limit*(1-c)*(Yd-Yc))
-
-      end do
-
-
-!     do the vertical advection step which will be used for prescribed
-!     vertical flow velocity and for settling of suspended matter.
-      do k=1,N
-         Y(k)=Y(k)-1./float(it)*dt*((cu(k)-cu(k-1))/h(k))
-      enddo
-
-   end do ! end of the iteration loop
-
-   return
-   end subroutine mossco_adv_center
-#endif
 
 end module gotm_transport_component
