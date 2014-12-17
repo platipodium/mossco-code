a0ddf7e0ad6d6effaf25cc38e13f15eaf3405694
Hassan Nasermoaddeli <ak2mnase@widar.Hamburg.baw.de>
2014-12-17 15:04:43 +0100

    adjust for accepting spatial macrofauna and microphytbenthos data.

--- src/drivers/microphytobenthos_class.F90
+++ src/drivers/microphytobenthos_class.F90
@@ -2,8 +2,10 @@ module Microphytobenthos_class
 
 ! The microphytobenthos class is a subclass of superclass Benthos_Effect. It comprises The effect of
 ! microphytobenthos on erodibility and crticial bed shear stress.
-
+use BioTypes
 use BenthosEffect_class
+use Bio_critical_shear_stress
+use Bio_erodibility
 
 implicit none
 type , public, extends (BenthosEffect) :: Microphytobenthos
@@ -33,7 +35,7 @@ integer                   :: istatus
 
 allocate (this%Species)
 allocate (this%BioMass)
-allocate (This%BioMass%amount)
+allocate (This%BioMass%amount(inum,jnum))
 allocate (this%TauEffect(inum,jnum))
 allocate (this%ErodibilityEffect (inum,jnum),stat= istatus)
 
@@ -47,21 +49,24 @@ allocate (this%ErodibilityEffect (inum,jnum),stat= istatus)
   this%jnum      = jnum
   this%TauEffect = 1.0_fp
   this%ErodibilityEffect = 1.0_fp
+  this%BioMass%amount    = 0._fp
 
 end subroutine init_microphyt
 
 subroutine set_microphyt (this)
-use BioTypes
+
 implicit none
 
 class (Microphytobenthos)  :: this
-real (fp)                  :: Mass
+real (fp), dimension (:,:), allocatable  :: Mass
 character (len = 10)       :: units
 integer                    :: StringLength, UnitNr, istat
 logical                    :: opnd, exst
 
 
 namelist /Microphyto/ units, Mass
+
+allocate ( Mass ( this%inum , this%jnum ) )
 
 this%Species='Microphytobenthos'
 
@@ -79,14 +84,14 @@ if (exst.and.(.not.opnd)) then
 
 elseif (opnd) then
 
- write (*,*) ' in Microphytobenthos the file unit ', UnitNr, ' already opened'
+ write (*,*) ' In Microphytobenthos the file unit ', UnitNr, ' already opened'
  read (UnitNr, nml=Microphyto, iostat = istat)
 
  if (istat /= 0 ) write (*,*) ' Error in reading Microphytobenthos data'
 
 else
 
- write (*,*) 'Warning: The input file for Microphytobenthos doesnot exists!'
+ write (*,*) ' Warning: The input file for Microphytobenthos doesnot exists!'
  write (*,*) ' Biological effects on erodibility and bed shear stress are set to 1.'
 
 end if
@@ -97,7 +102,7 @@ end if
 
  This%BioMass%amount = Mass
 
-StringLength = len_trim (units)
+ StringLength = len_trim (units)
 
 if (StringLength /= 0 ) then
     !allocate (character(StringLength) :: This%BioMass%units)
@@ -113,9 +118,6 @@ end subroutine set_microphyt
 
 
 subroutine run_microphyt (this)
-use Bio_critical_shear_stress
-use Bio_erodibility
-
 
 implicit none
 !#define DEBUG
@@ -123,17 +125,17 @@ class (Microphytobenthos) :: this
 
 integer                   :: i,j
 
-do i = 1, this%inum
- do j = 1, this%jnum
-     this%TauEffect (i,j)         =  Crit_shear_bioeffect(this%BioMass)
-     this%ErodibilityEffect (i,j) =  erodibility_bioeffect(this%BioMass)
- end do
-end do
+!do i = 1, this%inum
+ !do j = 1, this%jnum
+     this%TauEffect          =  Crit_shear_bioeffect(this%BioMass, this%inum, this%jnum)
+     this%ErodibilityEffect  =  erodibility_bioeffect(this%BioMass, this%inum, this%jnum)
+ !end do
+!end do
 
 #ifdef DEBUG
 write (*,*) ' Biotic effect of ', this%Species, ' on tau ( Micro%TauEffect) ', this%TauEffect
 write (*,*)
-write (*,*) 'Biotic effect of ' , this%Species, ' on the Erodibility (Micro%ErodibilityEffect): ', this%ErodibilityEffect
+write (*,*) ' Biotic effect of ' , this%Species, ' on the Erodibility (Micro%ErodibilityEffect): ', this%ErodibilityEffect
 write (*,*)
 #endif
 
