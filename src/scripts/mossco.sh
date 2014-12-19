27e8aa4e60614abd02801b26adfaab8420c41b11
Carsten Lemmen <carsten.lemmen@hzg.de>
2014-12-19 09:24:33 +0100

    Fixed whitespace errors in mossco.sh

--- src/scripts/mossco.sh
+++ src/scripts/mossco.sh
@@ -7,14 +7,14 @@ if [[ "x${1}" == "x" ]]; then ARG=benthic_geoecology ; else ARG=${1}; fi
 DIR=${MOSSCO_DIR}/examples/${ARG}
 test -d ${DIR} || exit 1
 
-test -f mossco_run.nml || exit 1
+test -f mossco_run.nml || exit 1
 TITLE=$(cat mossco_run.nml | grep title | cut -f2 -d "'")
 
 make -C ${DIR}
 
 EXE=${DIR}/${ARG}
 echo ${EXE}  ${TITLE}
-test -x ${EXE} || exit 1
+test -x ${EXE} || exit 1
 
 rm -rf PET?.${TITLE} ${TITLE}.stdout
 ${EXE} | tee ${TITLE}.stdout
