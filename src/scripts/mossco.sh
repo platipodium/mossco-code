8e4fa8ef8970fc3c8606ad28c57c82d2bf71e2ff
Carsten Lemmen <carsten.lemmen@hzg.de>
2015-01-13 22:14:51 +0100

    Bugfixes to mossco.sh script

--- src/scripts/mossco.sh
+++ src/scripts/mossco.sh
@@ -5,13 +5,13 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 GENERIC=0
 REMAKE=0
 
-while getopts "rg:" opt; do
-    case "$opt" in
-    r)  REMAKE=1
-        ;;
-    g)  GENERIC=1
-        ;;
-    esac
+while getopts "rg" opt; do
+  case "$opt" in
+  r)  REMAKE=1
+      ;;
+  g)  GENERIC=1
+      ;;
+  esac
 done
 
 shift $((OPTIND-1))
@@ -28,7 +28,6 @@ fi
 test -d ${DIR} || ( echo "ERROR, directory ${DIR} does not exist" ; exit 1)
 EXE=${DIR}/${ARG}
 
-
 if [[ ${GENERIC} == 1 ]] ; then
   if [[ ${REMAKE} == 0 ]] ; then
     test -x  ${EXE} || REMAKE=1
@@ -37,18 +36,20 @@ if [[ ${GENERIC} == 1 ]] ; then
     test -f ${DIR}/create_coupling.py || ( echo "ERROR, script create_coupling.py does not exist"; exit 1)
     test -f ${EXE}.yaml || ( echo "ERROR, coupling spec ${EXE}.yaml does not exist"; exit 1)
     (cd ${DIR}; python create_coupling.py ${ARG})
-  fi
-  make -C ${DIR}
-  test -x  ${EXE} || ( echo "ERROR, could not create executable ${EXE}" ; exit 1)
-else
-  if [[ ${REMAKE} == 0 ]] ; then
-    test -x  ${DIR}/${ARG} || REMAKE=1
-  fi
-  if  [[ ${REMAKE} == 1 ]] ; then
+    rm -f ${EXE}
     make -C ${DIR}
   fi
-  test -x  ${DIR}/${ARG} || ( echo "ERROR, could not create executable ${EXE}" ; exit 1)
+else
+  if [[ ${REMAKE} == 0 ]] ; then
+    test -x  ${EXE} || REMAKE=1
+  fi
+  if  [[ ${REMAKE} == 1 ]] ; then
+    rm -f ${EXE}
+    make -C ${DIR}
+  fi
 fi
+
+test -x  ${EXE} || ( echo "ERROR, could not create executable ${EXE}" ; exit 1)
 
 test -f mossco_run.nml || (echo "ERROR, need file mossco_run.nml to run" ; exit 1)
 TITLE=$(cat mossco_run.nml | grep title | cut -f2 -d "'")
