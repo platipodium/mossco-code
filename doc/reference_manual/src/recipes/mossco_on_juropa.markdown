095a61e004330499cadc8b0df5023402f3c693c9
Carsten Lemmen <carsten.lemmen@hzg.de>
2015-01-18 14:03:59 +0100

    Added options -s and -n to mossco.sh

--- doc/reference_manual/src/recipes/mossco_on_juropa.markdown
+++ doc/reference_manual/src/recipes/mossco_on_juropa.markdown
@@ -1,6 +1,6 @@
 # Recipe #10: MOSSCO on juropa
 
-Juropa is the supercomputer at FZ Jülich.  You can apply for a testaccount by contacting their support at <dispatch@fz-juelich.de>.
+Juropa is the supercomputer at FZ Jülich.  You can apply for a testaccount by contacting their support at <dispatch.jsc@fz-juelich.de>.
 
 Juropa offers ... everything
 
@@ -12,11 +12,12 @@ Load the appropriate modules (Intel 11 is the default, we need a higher version)
 	module load intel/13.1.3 parastation/mpi-intel13-5.1.0
 	module load netcdf autotools cmake git/1.8.3
 
-    export ESMFMKFILE=/home/lemmen/opt/lib/libg/Linux.intel.64.openmpi.ESMF_7_0_0_beta_snapshot_04/esmf.mk
+    export ESMFMKFILE=${HOME}/opt/esmf/lib/libO/Linux.intel.64.mpich2.default/esmf.mk
 
     export NETCDF=NETCDF4
     export NETCDF_VERSION=$NETCDF
     #export NETCDFINC=$(nc-config --includedir)
+    export FORTRAN_COMPILER=IFORT
 
 This example also assumes that you have downloaded or `git clone`ed MOSSCO into a directory referred to by the environment variable `$MOSSCO_DIR`
 
@@ -27,3 +28,20 @@ This example also assumes that you have downloaded or `git clone`ed MOSSCO into 
     make
 
 
+## Installing ESMF (if not done so already)
+
+You should not have to do this, it is documented here for administrators.
+
+	export ESMF_DIR=${HOME}/devel/ESMF/code
+    export ESMF_BOPT=g
+	export ESMF_OPTLEVEL=2
+	export ESMF_COMM=mpich2
+	export ESMF_NETCDF=split
+	export ESMF_INSTALL_PREFIX=${HOME}/opt/esmf
+	export ESMF_COMPILER=intel
+	
+	export ESMF_NETCDF_INCLUDE=$(nc-config --includedir)
+	export ESMF_NETCDF_LIBPATH=${ESMF_NETCDF_INCLUDE%%include}lib
+	export ESMF_F90COMPILEOPTS=-DESMF_NO_SEQUENCE
+
+	(cd $ESMF_DIR; make && make install)
--- src/scripts/mossco.sh
+++ src/scripts/mossco.sh
@@ -5,24 +5,29 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 GENERIC=0
 REMAKE=0
 BUILD_ONLY=0
+NP=1
 DEFAULT=benthic_geoecology
-
+SYSTEM=INTERACTIVE
 
 usage(){
 	echo "Usage: $0 [options] [example]"
-	echo "Accepted options are -r, -b, -g"
+	echo "Accepted options are -r, -b, -g, -n <numproc>, -s <system>"
 	echo "If not provided, the default example is ${DEFAULT}"
 	exit 1
 }
 
 
-while getopts "rgb" opt; do
+while getopts "rgbn:s:" opt; do
   case "$opt" in
   r)  REMAKE=1
       ;;
   g)  GENERIC=1
       ;;
   b)  BUILD_ONLY=1; REMAKE=1
+      ;;
+  n)  NP=${OPTARG}
+      ;;
+  s)  SYSTEM=${OPTARG}
       ;;
   \?) usage
       ;;
@@ -66,24 +71,91 @@ fi
 
 test -x  ${EXE} || ( echo "ERROR, could not create executable ${EXE}" ; exit 1)
 
-if [[ ${BUILD_ONLY} == 1 ]] ; then
-  exit 0
+case ${SYSTEM} in
+  moab|MOAB|M)  MPI_PREFIX="mpiexec"
+                SYSTEM=MOAB
+                ;;
+  sge|SGE|S)    MPI_PREFIX="mpirun"
+                SYSTEM=SGE
+                ;;
+  *)  MPI_PREFIX="mpirun "
+                ;;
+esac
+
+if [[ ${NP} == 0 ]]; then
+  MPI_PREFIX=""
+  NP=1
 fi
 
 test -f mossco_run.nml || (echo "ERROR, need file mossco_run.nml to run" ; exit 1)
 TITLE=$(cat mossco_run.nml | grep title | cut -f2 -d "'")
 
-echo ${EXE} ${TITLE}
+echo ${MPI_PREFIX} ${EXE} ${TITLE}
 
 STDERR=${ARG}-${TITLE}.stderr
 STDOUT=${ARG}-${TITLE}.stdout
 
-rm -rf PET?.${TITLE} ${TITLE}.stdout ${STDERR} ${STDOUT}
-${EXE} 1>  ${STDOUT} 2> ${STDERR}
+NODES=1
+PPN=${NP}
 
-tail -n 20 ${STDOUT}
-tail -n 20 ${STDERR}
-tail -n 100 PET0.${TITLE}
+case ${SYSTEM} in
+  MOAB)  NODES=$(expr \( $NP - 1 \) / 8 + 1 )
+         PPN=$(expr \( $NP - 1 \) / $NODES + 1 )
+         NP=$(expr $NODES \* $PPN )
+         ;;
+  *)     ;;
+esac
+
+MPI_PREFIX="${MPI_PREFIX} -np ${NP}"
+
+cat << EOT > moab.sh
+#!/bin/bash -x
+
+#MSUB -l nodes=${NODES}:ppn=${PPN}
+#MSUB -l walltime=0:30:00
+
+#MSUB -M carsten.lemmen@hzg.de
+#MSUB -m abe
+#MSUB -N ${TITLE}
+
+${MPI_PREFIX} ${EXE} > \$PBS_O_WORKDIR/\$PBSJOBID.stdout 2> \$PBS_O_WORKDIR/\$PBSJOBID.stderr
+EOT
+
+cat << EOT > sge.sh
+#!/bin/bash
+
+#$ -N ${TITLE}
+#$ -pe orte $NP
+#$ -cwd
+#$ -V
+
+cat \$PE_HOSTFILE
+
+#mkdir -p ${OUTDIR}
+#test -d ${OUTDIR} || (echo "Directory ${OUTDIR} could not be created" ; exit 1)
+
+${MPI_PREFIX} ${EXE} > ${STDOUT} 2> ${STDERR}
+EOT
+
+
+rm -rf PET?.${TITLE} ${TITLE}.stdout ${STDERR} ${STDOUT}
+
+if [[ ${BUILD_ONLY} == 1 ]] ; then
+  exit 0
+fi
+
+case ${SYSTEM} in
+  MOAB)  if test -x msub ; then msub moab.sh ; else cat moab.sh ; fi
+         ;;
+  SGE)   if test -x qsub ; then qsub sge.sh ; else cat sge.sh ; fi
+         ;;
+  *)  ${MPI_PREFIX} ${EXE}  1>  ${STDOUT}  2> ${STDERR}
+         ;;
+esac
+
+test -f ${STDOUT} && tail -n 20 ${STDOUT}
+test -f ${STDERR} && tail -n 20 ${STDERR}
+test -f PET0.${TITLE} && tail -n 100 PET0.${TITLE}
 
 
 
