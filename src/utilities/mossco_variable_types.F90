9ce0771d731b13acd701a6a1ffabed7857a3b575
Carsten Lemmen <carsten.lemmen@hzg.de>
2015-03-04 11:20:47 +0100

    Added valid_min, valid_max, missing_value to standard properties of mossco_variable

--- src/utilities/mossco_variable_types.F90
+++ src/utilities/mossco_variable_types.F90
@@ -2,48 +2,54 @@ module mossco_variable_types
 
 #ifdef ESMF
   use esmf
-#endif  
-  
+#endif
+
   implicit none
-  
-  
+
+
 
   type, public, abstract :: MOSSCO_VariableInfo
     character(len=255) :: name  = ''   ! Short name
     character(len=255) :: units = ''    ! Units
-    character(len=511) :: standard_name = '' ! CF standard name 
-    character(len=511) :: description = '' ! long description 
-    contains 
+    character(len=511) :: standard_name = '' ! CF standard name
+    character(len=511) :: description = '' ! long description
+    contains
       procedure :: as_yaml
-    
+
   end type
 
-#ifdef ESMF 
+#ifdef ESMF
 integer, parameter :: MOSSCO_KIND_R8=ESMF_KIND_R8
 #else
 integer, parameter :: MOSSCO_KIND_R8=selected_real_kind(13)
 #endif
 
-  type, extends(MOSSCO_VariableInfo), public :: MOSSCO_VariableFArray0d
+  type, extends(MOSSCO_VariableInfo), public :: MOSSCO_VariableFloat
+    real(MOSSCO_KIND_R8) :: valid_min = -1e30+1.0
+    real(MOSSCO_KIND_R8) :: valid_max = huge(1.0_MOSSCO_KIND_R8)
+    real(MOSSCO_KIND_R8) :: missing_value = -1.0e30
+  end type
+
+  type, extends(MOSSCO_VariableFloat), public :: MOSSCO_VariableFArray0d
      real(MOSSCO_KIND_R8), pointer :: data
      integer :: rank = 0
   end type
-  type, extends(MOSSCO_VariableInfo), public :: MOSSCO_VariableFArray1d
+  type, extends(MOSSCO_VariableFloat), public :: MOSSCO_VariableFArray1d
      real(MOSSCO_KIND_R8), pointer, dimension(:) :: data
-     integer :: rank=1
+     integer :: rank = 1
   end type
-  type, extends(MOSSCO_VariableInfo), public :: MOSSCO_VariableFArray2d
+  type, extends(MOSSCO_VariableFloat), public :: MOSSCO_VariableFArray2d
      real(MOSSCO_KIND_R8), pointer, dimension(:,:) :: data
-     integer :: rank=2
-     
+     integer :: rank = 2
+
   end type
-  type, extends(MOSSCO_VariableInfo), public :: MOSSCO_VariableFArray3d
+  type, extends(MOSSCO_VariableFloat), public :: MOSSCO_VariableFArray3d
      real(MOSSCO_KIND_R8), pointer, dimension(:,:,:) :: data
-     integer :: rank=3
+     integer :: rank = 3
   end type
-  type, extends(MOSSCO_VariableInfo), public :: MOSSCO_VariableFArray4d
+  type, extends(MOSSCO_VariableFloat), public :: MOSSCO_VariableFArray4d
      real(MOSSCO_KIND_R8), pointer, dimension(:,:,:,:) :: data
-     integer :: rank=4
+     integer :: rank = 4
   end type
 
 contains
@@ -51,13 +57,13 @@ contains
 subroutine as_yaml(self)
 
   class(MOSSCO_VariableInfo) :: self
-  
+
   if (len(self%name) > 0) then
     write(*,'(A)') self%name
     if (len(self%standard_name) > 0) write(*,'(A,A)') '  standard_name:',self%standard_name
     if (len(self%description) > 0) write(*,'(A,A)') '  description:',self%description
-  endif  
-  
+  endif
+
 end subroutine as_yaml
 
 end module mossco_variable_types
