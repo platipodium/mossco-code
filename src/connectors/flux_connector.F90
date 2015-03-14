05a3651071eb629c58b75b2ee5735c781486cb22
Carsten Lemmen <carsten.lemmen@hzg.de>
2015-03-14 11:07:58 +0100

    Added functionality to flux_conn

--- src/connectors/flux_connector.F90
+++ src/connectors/flux_connector.F90
@@ -113,15 +113,7 @@ module flux_connector
     type(ESMF_Time)       :: currTime
     integer(ESMF_KIND_I4) :: localrc
 
-    !! Check whether there is already a clock (it might have been set
-    !! with a prior ESMF_cplCompCreate() call.  If not, then create
-    !! a local clock as a clone of the parent clock, and associate it
-    !! with this component.  Finally, set the name of the local clock
     call MOSSCO_CompEntry(cplComp, parentClock, name, currTime, localrc)
-    if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
-      call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
-
-    call MOSSCO_AddImportFluxToExportState(importState, exportState, localrc)
     if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
       call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
 
@@ -144,14 +136,11 @@ module flux_connector
     type(ESMF_Time)         :: currTime, stopTime
     type(ESMF_Clock)        :: clock
     integer(ESMF_KIND_I4)   :: localrc
+    type(ESMF_TimeInterval) :: timeStep
 
     rc=ESMF_SUCCESS
 
     call MOSSCO_CompEntry(cplComp, parentClock, name, currTime, localrc)
-    if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
-      call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
-
-    call MOSSCO_AddImportFluxToExportState(importState, exportState, localrc)
     if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
       call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
 
@@ -163,7 +152,13 @@ module flux_connector
     if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
       call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
 
-    call ESMF_ClockAdvance(clock, timeStep=stopTime-currTime, rc=localrc)
+    timeStep=stopTime - currTime
+
+    call MOSSCO_AddImportFluxToExportState(importState, exportState, timeStep)
+    if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
+      call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+    call ESMF_ClockAdvance(clock, timeStep=timeStep, rc=localrc)
     if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc)) &
       call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
 
@@ -201,5 +196,141 @@ module flux_connector
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT, rc=rc)
 
   end subroutine Finalize
+
+#undef  ESMF_METHOD
+#define ESMF_METHOD "Finalize"
+  subroutine MOSSCO_AddImportFluxToExportState(importState, exportState, timeStep, rc)
+
+    type(ESMF_State)                       :: importState, exportState
+    type(ESMF_TimeInterval)                :: timeStep
+    integer(ESMF_KIND_I4), optional        :: rc
+
+    type(ESMF_Field)                       :: exportField
+    type(ESMF_Field)                       :: rateField
+    character(ESMF_MAXSTR), allocatable    :: itemNameList(:)
+    character(ESMF_MAXSTR)                 :: itemName, message
+    type(ESMF_StateItem_Flag), allocatable :: itemTypeList(:)
+    type(ESMF_StateItem_Flag)              :: itemType
+    type(ESMF_FieldStatus_Flag)            :: fieldStatus
+    integer(ESMF_KIND_I4)                  :: i, j, k, l, nmatch, itemCount, rank, externalIndex
+    integer(ESMF_KIND_I4)                  :: ubnd2(2), lbnd2(2)
+
+    real(ESMF_KIND_R8)                     :: dt
+    real(ESMF_KIND_R8), pointer            :: farrayPtr3(:,:,:), ratePtr3(:,:,:)
+    real(ESMF_KIND_R8), pointer            :: farrayPtr2(:,:), ratePtr2(:,:)
+    logical                                :: isPresent
+
+    integer(ESMF_KIND_I4)                  :: localrc, rc_
+
+    call ESMF_StateGet(importState, itemCount=itemCount, rc=localrc)
+    if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+      call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+    allocate(itemNameList(itemCount))
+    allocate(itemTypeList(itemCount))
+    call ESMF_StateGet(importState, itemNameList=itemNameList, itemTypeList=itemTypeList, rc=localrc)
+    if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+      call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+    call ESMF_TimeIntervalGet(timeStep, s_r8=dt, rc=localrc)
+    if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+      call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+    do i=1, itemCount
+
+      j=index(itemNameList(i),'_flux_in_water')
+      if (j<1) cycle
+
+      itemName=itemNameList(i)
+      call ESMF_StateGet(exportState, itemName(1:j)//'in_water', itemType=itemType, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      if (itemType /= ESMF_STATEITEM_FIELD) cycle
+
+      call ESMF_StateGet(exportState, itemName(1:j)//'in_water', exportField, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      call ESMF_FieldGet(exportField, status=fieldStatus, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      if (fieldStatus /= ESMF_FIELDSTATUS_COMPLETE) cycle
+
+      call ESMF_AttributeGet(exportField, 'external_index', isPresent=isPresent, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      if (.not.isPresent) then
+        externalIndex = -1
+      else
+        call ESMF_AttributeGet(exportField, 'external_index', externalIndex, rc=localrc)
+        if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+          call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+      endif
+
+      if (itemTypeList(i) /= ESMF_STATEITEM_FIELD) cycle
+
+      call ESMF_StateGet(importState, trim(itemName), rateField, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      call ESMF_FieldGet(rateField, status=fieldStatus, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      if (fieldStatus /= ESMF_FIELDSTATUS_COMPLETE) cycle
+
+      call ESMF_FieldGet(exportField, farrayPtr=farrayPtr3, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      call ESMF_FieldGet(rateField, rank=rank, rc=localrc)
+      if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+        call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+      if (rank==2) then
+        call ESMF_FieldGetBounds(rateField, exclusiveUBound=ubnd2, exclusiveLBound=lbnd2, rc=localrc)
+        if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+          call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+        call ESMF_FieldGet(rateField, farrayPtr=ratePtr2, rc=localrc)
+        if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+          call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+        do k=1,ubound(farrayPtr3, 3)
+          do j=lbnd2(2),ubnd2(2)
+            do l=lbnd2(1), ubnd2(1)
+              if (ratePtr2(l,j)>0) farrayPtr3(l,j,k) = farrayPtr3(l,j,k)  + ratePtr2(l,j) * dt
+            enddo
+          enddo
+        enddo
+      elseif (rank==3) then
+        call ESMF_FieldGet(rateField(i), farrayPtr=ratePtr3, rc=localrc)
+        if (ESMF_LogFoundError(localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=rc_)) &
+          call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+        where(ratePtr3(:,:,:)>-1)
+          farrayPtr3(:,:,:) = farrayPtr3(:,:,:)  + ratePtr3(:,:,:) * dt
+        endwhere
+      endif
+
+      write(message,'(A)') '  added '
+      call MOSSCO_FieldString(exportField, message)
+      write(message,'(A)') trim(message)//' to '
+      call MOSSCO_FieldString(rateField, message)
+      call ESMF_LogWrite(trim(message), ESMF_LOGMSG_INFO)
+    enddo
+
+    if (allocated(itemNameList))    deallocate(itemNameList)
+    if (allocated(itemTypeList))    deallocate(itemTypeList)
+
+    if (present(rc)) rc=rc_
+
+    return
+
+  end subroutine MOSSCO_AddImportFluxToExportState
+
 
 end module flux_connector
