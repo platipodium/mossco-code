43b8c1b80ae2048bd244bcd62b2720c05d2e2bdb
Onur Kerimoglu <onur.kerimoglu@hzg.de>
2015-03-23 17:24:59 +0100

    postprocessing: new script to make system-specific calls to process_fabmout.sh

---
+++ src/scripts/postprocess/call_postprocessing.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+#
+# This script  is part of MOSSCO.  It attempts to call the postprocessing script correctly
+# use: process_fabmout.sh mossco_gffn.00.nc
+#
+# @copyright (C) 2015 Helmholtz-Zentrum Geesthacht
+# @authors: onur.kerimoglu@hzg.de
+#
+# MOSSCO is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License v3+.  MOSSCO is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY.  Consult the file
+# LICENSE.GPL or www.gnu.org/licenses/gpl-3.0.txt for the full license terms.
+
+system=$1 #todo: doing it as a slurm or sge command on cluster
+outdir=$2 #todo: detect the outputdir using the mossco_gffn.cfg file (?)
+
+proc_script=$MOSSCO_DIR/src/scripts/postprocess/process_fabmout.sh
+outdir=/data/kerimogl/mossco-results/nsbs/nsbs47-3r
+fnameroot=mossco_gffn
+user=kerimogl
+remotemachine=grpsrv05
+
+#attempt to detect whether ncks is available locally
+#if $(which ncks 2> /dev/null) ; then
+if false ; then
+	#do processing locally
+	process_fabmout.sh $toolpath $outdir $fnameroot
+else
+	#do processing through a remote machine
+        outdir=$(echo $outdir | sed 's/data/ocean-data/')
+	MOSSCO_DIR=$(echo $MOSSCO_DIR | sed 's/home/ocean-home/')
+        proc_script=$(echo $proc_script | sed 's/home/ocean-home/')
+	echo '(remote) outdir:'$outdir
+ 	echo '(remote) proc_script:'$proc_script  	
+	ssh ${user}@${remotemachine} "export LD_LIBRARY_PATH=/usr/local/lib:\$LD_LIBRARY_PATH; export PATH=/usr/local/bin/:\$PATH; $proc_script $outdir $fnameroot $MOSSCO_DIR" 
+fi
