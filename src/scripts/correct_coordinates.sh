b2b6fe73fbbaa9d1cb52dbac15fe7190b587e952
Carsten Lemmen <carsten.lemmen@hzg.de>
2015-05-27 13:23:07 +0200

    Added scritp for correcting coordinates

---
+++ src/scripts/correct_coordinates.sh
@@ -0,0 +1,21 @@
+T="$1"
+S=../coordinates/mossco_gffn
+
+
+for F in $T ; do
+	test -f $F || continue
+
+  B=${F%%.nc}
+  I=${B##*.}
+  B=${B%%.*}
+
+  ncks -A -v getmGrid2D_getm_lat,getmGrid2D_getm_lon ${S}.${I}.nc $F
+  ncap2  -A -s "getmGrid3D_getm_lat=getmGrid2D_getm_lat" $F $F
+  ncap2  -A -s "getmGrid3D_getm_lon=getmGrid2D_getm_lon" $F $F
+
+  echo $F
+
+done
+
+
+
