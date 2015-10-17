eaff877829e8fb65654f0668c00be6451f95d929
Knut <knutaros@gmx.net>
2015-10-18 00:57:38 +0200

    getm: export "dissipation_of_tke_at_soil_surface"

--- src/components/getm_component.F90
+++ src/components/getm_component.F90
@@ -75,7 +75,7 @@ module getm_component
   real(ESMF_KIND_R8),pointer :: T3D(:,:,:)=>NULL()
   real(ESMF_KIND_R8),pointer :: S3D(:,:,:)=>NULL()
   real(ESMF_KIND_R8),pointer :: swr(:,:)=>NULL()
-  real(ESMF_KIND_R8),pointer :: nybot(:,:)=>NULL(),tkebot(:,:)=>NULL()
+  real(ESMF_KIND_R8),pointer :: nybot(:,:)=>NULL(),tkebot(:,:)=>NULL(),epsbot(:,:)=>NULL()
   real(ESMF_KIND_R8),pointer :: windU(:,:)=>NULL(),windV(:,:)=>NULL()
   real(ESMF_KIND_R8),pointer :: waveH(:,:)=>NULL(),waveT(:,:)=>NULL(),waveK(:,:)=>NULL(),waveDir(:,:)=>NULL()
 
@@ -392,6 +392,9 @@ module getm_component
     end if
     if (associated(tkebot)) then
       call getmCmp_StateAddPtr("turbulent_kinetic_energy_at_soil_surface",tkebot,exportState,"m2 s-2",name)
+    end if
+    if (associated(epsbot)) then
+      call getmCmp_StateAddPtr("dissipation_of_tke_at_soil_surface",epsbot,exportState,"m2 s-3",name)
     end if
 
     select case (met_method)
@@ -834,7 +837,7 @@ module getm_component
    use initialise     ,only: runtype
    use variables_2d   ,only: D
 #ifndef NO_3D
-   use variables_3d   ,only: hn,num,tke
+   use variables_3d   ,only: hn,num,tke,eps
 #ifndef NO_BAROCLINIC
    use m3d            ,only: calc_temp,calc_salt
    use variables_3d   ,only: T,S
@@ -914,6 +917,7 @@ module getm_component
          allocate(hbot(E2DFIELD))
          allocate(nybot(I2DFIELD))
          allocate(tkebot(I2DFIELD))
+         allocate(epsbot(I2DFIELD))
 #ifndef NO_BAROCLINIC
          if (calc_temp) then
             allocate(Tbot(I2DFIELD))
@@ -985,9 +989,11 @@ module getm_component
 #if 0
          nybot (imin-HALO:,jmin-HALO:) => num(:,:,1)
          tkebot(imin-HALO:,jmin-HALO:) => tke(:,:,1)
+         epsbot(imin-HALO:,jmin-HALO:) => eps(:,:,1)
 #else
          allocate(nybot (I2DFIELD))
          allocate(tkebot(I2DFIELD))
+         allocate(epsbot(I2DFIELD))
 #endif
 #ifndef NO_BAROCLINIC
          if (calc_temp) then
@@ -1908,7 +1914,7 @@ module getm_component
    use initialise     ,only: runtype
    use variables_2d   ,only: zo,z,D,Dvel,U,DU,V,DV
 #ifndef NO_3D
-   use variables_3d   ,only: dt,ho,hn,hvel,uu,hun,vv,hvn,ww,num,tke
+   use variables_3d   ,only: dt,ho,hn,hvel,uu,hun,vv,hvn,ww,num,tke,eps
 #ifndef NO_BAROCLINIC
    use m3d            ,only: calc_temp,calc_salt
    use variables_3d   ,only: T,S
@@ -1954,6 +1960,7 @@ module getm_component
          hbot   = hn (:,:,1)
          nybot  = num(:,:,1)
          tkebot = tke(:,:,1)
+         epsbot = eps(:,:,1)
 #ifndef NO_BAROCLINIC
          if (calc_temp) then
             Tbot = T(:,:,1)
@@ -1987,6 +1994,7 @@ module getm_component
       if (runtype .gt. 1) then
          nybot  = num(:,:,1)
          tkebot = tke(:,:,1)
+         epsbot = eps(:,:,1)
       end if
 #endif
 #endif
