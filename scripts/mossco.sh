3a8a107f7b415ff66b483748a65037407f10dc7c
Knut <knutaros@gmx.net>
2015-12-03 00:32:23 +0100

    mossco.sh: support multiple spaces between nml parameter and =

--- scripts/mossco.sh
+++ scripts/mossco.sh
@@ -465,26 +465,26 @@ if [[ ${RETITLE} != 0 ]] ; then
   fi
 
   if test -f getm.inp ; then
-    ${SED} -i "s/runid =.*/runid = '${TITLE}',/" getm.inp
+    ${SED} -i "s/runid *=.*/runid = '${TITLE}',/" getm.inp
     export runid="${TITLE}"
     if [[ "x${MPI_PREFIX}" != "x" ]] ; then
-      ${SED} -i 's/parallel =.*/parallel = .true.,/' getm.inp
+      ${SED} -i 's/parallel *=.*/parallel = .true.,/' getm.inp
       export parallel=True
     else
-      ${SED} -i 's/parallel =.*/parallel = .false.,/' getm.inp
+      ${SED} -i 's/parallel *=.*/parallel = .false.,/' getm.inp
       export parallel=False
     fi
   fi
 
   if test -f gotmrun.nml ; then
-    ${SED} -i "s/title =.*/title = '${TITLE}',/" gotmrun.nml
+    ${SED} -i "s/title *=.*/title = '${TITLE}',/" gotmrun.nml
     export title="${TITLE}"
-    ${SED} -i "s/out_fn =.*/out_fn = '${TITLE}_gotm',/" gotmrun.nml
+    ${SED} -i "s/out_fn *=.*/out_fn = '${TITLE}_gotm',/" gotmrun.nml
     export out_fn="${TITLE}_gotm"
   fi
 
   if test -f mossco_run.nml ; then
-    ${SED} -i "s/title =.*/title = '${TITLE}',/" mossco_run.nml
+    ${SED} -i "s/title *=.*/title = '${TITLE}',/" mossco_run.nml
     export title="${TITLE}"
   fi
 fi
