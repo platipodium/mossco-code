b5c7a6359c3f46a45099fb874add3806a611531a
Kai Wirtz <kai.wirtz@hzg.de>
2016-06-09 22:40:21 +0200

    copy setups adapted to new sns structure

--- scripts/kai/cp_setup_dir.sh
+++ scripts/kai/cp_setup_dir.sh
@@ -14,14 +14,16 @@ tags='-N'
 mkdir -p $dir2
 cd  $dir1
 
-for a in `ls --ignore="mossco*.nc" --ignore="sns*.nc" --ignore="boundary*.nc"--ignore="restart.00*" --ignore="*.std*" --ignore="PET*"`; do cp -d $a ../$dir2; done
+for a in `ls --ignore="mossco*.nc" --ignore="sns*.nc" --ignore="silt*.nc" --ignore="boundary*.nc"--ignore="restart.00*" --ignore="*.std*" --ignore="PET*"`; do cp -d $a ../$dir2; done
 cp -d sns_climatology.nc ../$dir2
+cp -rp Configuration ../$dir2
 cd  ../$dir2
 ln -s ../$dir1/Topo
-ln -s ../$dir1/Configurations
+#ln -s ../$dir1/Configurations
 ln -s ../$dir1/Forcing
 ln -s ../$dir1/restart
-ln -s ../$dir1/windmussel 
+ln -s ../$dir1/Parallel
+#ln -s ../$dir1/windmussel 
 #cp -rp ../$dir1/windmussel .
 
 ln=`awk '/'${tags}'/{print NR}' ${rn}`
