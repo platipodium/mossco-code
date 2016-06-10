555b3ab70cacc339c75d2bb26dc6b950f491a50a
Carsten Lemmen <carsten.lemmen@hzg.de>
2016-06-10 17:51:51 +0200

    Added grid (x) component to catalog

---
+++ examples/generic/Readme.md
@@ -0,0 +1,7 @@
+This directory contains generic example applications for a coupled MOSSCO system
+
+Please specify your coupling in a yaml file (see examples), and run
+
+  python  create_coupling.py <file.yaml>
+
+script with this yaml file as your first argument.
--- examples/generic/create_short_examples.py
+++ examples/generic/create_short_examples.py
@@ -13,7 +13,6 @@ import os
 import glob
 import re
 
-
 def wordreplace(filename, search, replace):
     with open(filename, "r+" ) as f:
         content = f.read()
@@ -35,40 +34,39 @@ fid = file(filename,'rU')
 catalog = yaml.safe_load(fid)
 fid.close()
 
-
 files_to_process = glob.glob('*--*.yaml')
 
 for i, filename in enumerate(files_to_process):
- 
+
     basename = os.path.basename(os.path.splitext(filename)[0])
-    
+
     items = basename.split('--')
     shortname=''
-    
+
     for j, item in enumerate(items):
         if catalog.has_key(item):
             shortname += catalog[item]
-            
+
     if len(shortname) != j + 1: continue
     linkname=shortname + '.yaml'
-        
+
     if os.path.isfile(linkname): os.remove(linkname)
-        
+
     try:
       os.symlink(filename, linkname)
     except:
       print 'Some error occured when linking ' + linkname + ' to ' + filename
-      
+
     print 'Linked ' + linkname + ' to ' + filename
 
     fid = file(linkname,'rU')
     config = yaml.safe_load(fid)
     fid.close()
-    
+
     if not config.has_key('instances'): continue
-        
+
     ncInstanceList=[]
-    if type(config['instances']) is dict: 
+    if type(config['instances']) is dict:
       for key, value in config['instances'].iteritems():
         if value is dict:
             if value.values().index('netcdf') > -1:
@@ -79,7 +77,7 @@ for i, filename in enumerate(files_to_process):
       for j, item in enumerate(config['instances']):
           for key, value in item.iteritems():
             if value == 'netcdf': ncInstanceList.append(key)
- 
+
     if not len(ncInstanceList) == 1: continue
-        
+
     wordreplace(filename,ncInstanceList[0],'mossco_'+shortname)
