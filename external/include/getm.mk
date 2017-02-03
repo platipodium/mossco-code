52ba4331cb258545d3df2885264a0c19141e79a9
Carsten Lemmen <carsten.lemmen@hzg.de>
2017-02-03 10:49:00 +0100

    Added comment

--- external/include/getm.mk
+++ external/include/getm.mk
@@ -49,6 +49,7 @@ endif
 
 getm_version:
 ifneq ($(wildcard $(external_GETMDIR)/src/Makefile),)
+  # git describe --long --tags --dirty --always
 	GETM_VERSION=$(shell cat /Users/lemmen/devel/MOSSCO/code/external/getm/code/VERSION)
 	GETM_GIT_SHA=$(shell cd $(external_GETMDIR) ; $(GIT) log -n 1 |head -1 | cut -d" " -f2)
 
