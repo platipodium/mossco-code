ee31b8d4fd1cdccc2a03bc50725ca31fa2394125
Carsten Lemmen <carsten.lemmen@hzg.de>
2017-02-11 13:35:30 +0100

    Updated copyright year

--- examples/common/main.F90
+++ examples/common/main.F90
@@ -2,7 +2,7 @@
 !> @file main.F90
 !!
 !> This computer program is part of MOSSCO.
-!> @copyright Copyright (C) 2013, 2014, 2015, 2016 Helmholtz-Zentrum Geesthacht
+!> @copyright Copyright (C) 2013, 2014, 2015, 2016, 2017 Helmholtz-Zentrum Geesthacht
 !> @author Carsten Lemmen <carsten.lemmen@hzg.de>
 !> @author Knut Klingbeil <knut.klingbeil@io-warnemuende.de>
 !> @author Richard Hofmeister <richard.hofmeister@hzg.de>
