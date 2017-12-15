749eaac4877fe9f9d09618f054c1e5288e9d7836
Carsten Lemmen <carsten.lemmen@hzg.de>
2017-12-15 16:56:19 +0100

    More work on test restart

--- src/test/restart/test_restart.sh
+++ src/test/restart/test_restart.sh
@@ -13,18 +13,31 @@ EOT
 
 cat << EOT > mossco_run.nml
 &mossco_run
- title = 'test_regrid',
+ title = 'test_restart',
  start = '2013-07-01 00:00:00',
  stop = '2013-07-01 10:00:00',
 /
 EOT
 
 ln -sf $MOSSCO_SETUPDIR/sns/getm.inp .
-ln -sf $MOSSCO_SETUPDIR/sns/meteofile.dat .
+grep 2013 $MOSSCO_SETUPDIR/sns/meteofiles.dat > meteofiles.dat
 ln -sf $MOSSCO_SETUPDIR/sns/parallel.inp .
 mkdir -p Parallel
 ln -sf $MOSSCO_SETUPDIR/sns/Parallel/par_setup* Parallel/
+ln -sf $MOSSCO_SETUPDIR/sns/Topo/topo.nc .
+ln -sf $MOSSCO_SETUPDIR/sns/bdy.2d.nc .
+ln -sf $MOSSCO_SETUPDIR/sns/bdy.3d.nc .
+ln -sf $MOSSCO_SETUPDIR/sns/sns_climatology.nc .
+ln -sf $MOSSCO_SETUPDIR/sns/rivers.nc .
+ln -sf $MOSSCO_SETUPDIR/sns/riverinfo.dat .
+ln -sf $MOSSCO_SETUPDIR/sns/bdyinfo.dat .
+ln -sf $MOSSCO_SETUPDIR/sns/gotmturb.nml .
 
 mossco -bn 1 test_restart
 
-mossco -sF -n12 test_restart
+for n in 12 ; do #16 22 32 61 83 96 117 144 158 178 192 288; do
+
+  ln -sf Parallel/par_setup.${n}p*dat par_setup.dat
+
+  mossco -n${n} -b -z 00:05:00 test_restart
+done
