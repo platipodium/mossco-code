cf632cefb6957b3136b9407c4e7dc1d33ab7d7f1
Carsten Lemmen <carsten.lemmen@hzg.de>
2018-02-17 12:11:16 +0100

    Cleaned up user interface of install esmf script

--- scripts/install/install_esmf_versions.sh
+++ scripts/install/install_esmf_versions.sh
@@ -1,49 +1,85 @@
-#!/bin/bash
+#!/bin/bash -x
+# This script is part of MOSSCO.  It helps in installing ESMF
+#
+# @copyright (C) 2013, 2014, 2015, 2016, 2017, 2018 Helmholtz-Zentrum Geesthacht
+# @author Carsten Lemmen <carsten.lemmen@hzg.de>
+#
+# MOSSCO is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License v3+.  MOSSCO is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY.  Consult the file
+# LICENSE.GPL or www.gnu.org/licenses/gpl-3.0.txt for the full license terms.
+#
 
-export TAGS=ESMF_7_1_0_beta_snapshot_52
-# The first optional argument is the base configuration dir, where the
-# environment variable file .esmf_.... is stored, and where the ESMF_DIR
-# is configured if not set
+# The script honours the following environment variables, and chooses
+# default values if these are not set
+# - ESMF_CONFIG_DIR
+# - ESMF_TAGS
+# - ESMF_COMPILERS
+# - ESMF_COMMUNICATORS
+# - ESMF_INSTALL_PREFIX
 
-if [ "x$1" == "x" ]; then
-  export CONFIG_DIR=${HOME}
+# Determine where to save the ESMF configuration.  Default is $HOME
+if [ -z ${ESMF_CONFIG_DIR} ]; then
+  CONFIG_DIR=${HOME}
 else
-  export CONFIG_DIR=${1}
+  CONFIG_DIR=${ESMF_CONFIG_DIR}
 fi
-
 test -d ${CONFIG_DIR} || mkdir -p ${CONFIG_DIR}
+echo "  $0 saves ESMF configuration in ${CONFIG_DIR}"
 
-# Environment variables COMPS, COMMS, ESMF_DIR and ESMF_INSTALL_PREFIX are honored
-# or filled with default values here
+test -z ${ESMF_DIR} && ESMF_DIR=${CONFIG_DIR}/devel/esmf-code
+echo "  $0 uses ESMF sources in ${ESMF_DIR}"
 
-test -n ${COMPS} || export COMPS="gfortranclang" #"gfortran intel pgi gfortranclang pgigcc intelgcc
-test -n ${COMMS} || export COMMS="openmpi" #"openmpi" # openmpi" #"openmpi" #  mpiuni mpich2 intelmpi
+# Determine what git tags of the ESMF repo are used.  Prefer the most
+# recent one (we need >= 7_1_0_beta_snapshot_52 for regridding with extrapolation)
+if [ -z "${ESMF_TAGS}" ]; then
+  TAGS=ESMF_7_1_0_beta_snapshot_51
+else
+  TAGS=${ESMF_TAGS}
+fi
+echo "  $0 installs ESMF for tags ${TAGS}"
 
-test -n ${ESMF_DIR} || export ESMF_DIR = ${CONFIG_DIR}/devel/esmf-code
+# Determine what compilers  are used.  We often test gfortran, gfortranclang,
+# and intel
+if [ -z "${ESMF_COMPILERS}" ]; then
+  COMPS=gfortranclang # gfortran intel pgi gfortranclang pgigcc intelgcc
+else
+  COMPS="${ESMF_COMPILERS}"
+fi
+echo "  $0 installs ESMF for compilers ${COMPS}"
+
+# Determine what communicators  are used.  We often test openmpi and intelmpi
+if [ -z "${ESMF_COMMUNICATORS}" ]; then
+  COMMS=openmpi # openmpi mpiuni mpich2 intelmpi
+else
+  COMMS="${ESMF_COMMUNICATORS}"
+fi
+echo "  $0 installs ESMF for communicators ${COMPS}"
+
+# Determine what ESMF installation directory
+if [ -z "${ESMF_INSTALL_PREFIX}" ]; then
+  ESMF_INSTALL_PREFIX=${CONFIG_DIR}/opt
+fi
+mkdir -p ${ESMF_INSTALL_PREFIX}/etc
+echo "  $0 installs ESMF to ${ESMF_INSTALL_PREFIX}"
 
 if test -d ${ESMF_DIR} ; then
-  echo "Using existing ESMF installation in $ESMF_DIR"
+  echo "  $0 uses existing ESMF installation in $ESMF_DIR"
 else
-  echo "Using new ESMF installation in $ESMF_DIR"
+  echo " $0 pulls new ESMF installation in $ESMF_DIR"
   git clone --depth=1 git://esmf.git.sourceforge.net/gitroot/esmf/esmf ${ESMF_DIR}
 fi
-
-test -n ${ESMF_INSTALL_PREFIX} || export ESMF_INSTALL_PREFIX=/opt/esmf
-mkdir -p ${ESMF_INSTALL_PREFIX}/etc
 
 export ESMF_OS=$(uname -s)
 export ESMF_ABI=64
 
+echo "  $0 uses  ESMF_OS=${ESMF_OS} and ESMF_ABI=${ESMF_ABI}"
+
 SED=sed
+test -z $(which sed) || SED=$(which sed)
+test -z $(which gsed) || SED=$(which gsed)
 
-echo Using SED=${SED}
-echo Using ESMF_OS=${ESMF_OS}
-echo Using ESMF_INSTALL_PREFIX=${ESMF_INSTALL_PREFIX}
-echo Using ESMF_DIR=${ESMF_DIR}
-echo Using compilers ${COMMS}
-echo Using communicators ${COMPS}
-
-#echo y        | module clear
+echo "  $0 uses  SED=${SED}"
 
 cd ${ESMF_DIR}
 
@@ -51,21 +87,20 @@ git stash && git stash drop
 git pull origin master
 
 for C in $COMMS ; do
-  echo "Iterating for Communicator $C "
+  echo "  $0 iterates for communicator $C "
   ESMF_COMM=$C
 
   for G in $COMPS; do
-    echo "Iterating for Compiler $G "
+    echo "  $0 iterates for Compiler $G "
     ESMF_COMPILER=$G
 
-    ESMF_NETCDF_INCLUDE=$(bash nc-config --includedir)
-    ESMF_NETCDF_LIBPATH=${ESMF_NETCDF_INCLUDE%%include}lib
+    ESMF_NETCDF_INCLUDE=/usr/include
 
-    NCCONFIG=$(which nc-config)
-    if [[ "x$NCCONFIG" == "x" ]] ; then
-      ESMF_NETCDF_INCLUDE=/usr/include
-      ESMF_NETCDF_LIBPATH=/usr/lib
+    if ! [ -z $(which nc-config) ]; then
+      ESMF_NETCDF_INCLUDE=$(bash nc-config --includedir)
     fi
+
+    ESMF_NETCDF_LIBPATH=${ESMF_NETCDF_INCLUDE%%include}lib
 
     if [ $(hostname) = ocean-fe.fzg.local ]; then
       ESMF_NETCDF_INCLUDE=/opt/netcdf/3.6.2/${G}/include
@@ -89,13 +124,13 @@ for C in $COMMS ; do
 #    module list
 
     if test -d ${ESMF_NETCDF_INCLUDE} ; then
-      echo using netcdf include ${ESMF_NETCDF_INCLUDE}
+      echo "  $0 uses netcdf include ${ESMF_NETCDF_INCLUDE}"
     else
       continue
     fi
 
     if test -d ${ESMF_NETCDF_LIBPATH} ; then
-      echo using netcdf libdir ${ESMF_NETCDF_LIBPATH}
+      echo "  $0 uses netcdf libdir ${ESMF_NETCDF_LIBPATH}"
     else
       continue
     fi
@@ -106,7 +141,6 @@ for C in $COMMS ; do
        ESMF_STRING=${ESMF_OS}.${ESMF_COMPILER}.${ESMF_ABI}.${ESMF_COMM}.${ESMF_SITE}
        git stash && git stash drop
        git checkout  -f $T
-
 
        rm -f ${ESMF_DIR}/build_config/${ESMF_OS}.${ESMF_COMPILER}.${ESMF_SITE}
        cp -r ${ESMF_DIR}/build_config/${ESMF_OS}.${ESMF_COMPILER}.default ${ESMF_DIR}/build_config/${ESMF_OS}.${ESMF_COMPILER}.${ESMF_SITE}
@@ -120,7 +154,7 @@ for C in $COMMS ; do
          sed 's#-openmp#-qopenmp#g' ${ESMF_DIR}/build_config/${ESMF_OS}.intel.default/build_rules.mk  > ${ESMF_DIR}/build_config/${ESMF_OS}.${ESMF_COMPILER}.${ESMF_SITE}/build_rules.mk
        fi
 
-       echo ESMFMKFILE=$ESMF_INSTALL_PREFIX/lib/libg/$ESMF_STRING/esmf.mk
+       echo ESMFMKFILE=${ESMF_INSTALL_PREFIX}/lib/libg/$ESMF_STRING/esmf.mk
        #test -f $ESMFMKFILE && continue
 
 cat << EOT > $CONFIG_DIR/.esmf_${ESMF_STRING}
