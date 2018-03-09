5861c4b8ed1b8edab136812ac576620e1360a9de
Carsten Lemmen <carsten.lemmen@hzg.de>
2018-03-09 23:05:01 +0100

    Removed debugging statements in regrid_coupler

--- src/mediators/regrid_coupler.F90
+++ src/mediators/regrid_coupler.F90
@@ -202,9 +202,6 @@ module regrid_coupler
       elseif (trim(geomFileFormatString) == 'GRIDSPEC') then
         if (hasMaskVariable) then
 
-          write(message, '(A)') trim(name)//' creating grid WITH mask ' //trim(mask_variable)
-          call ESMF_LogWrite(trim(message), ESMF_LOGMSG_ERROR, ESMF_CONTEXT)
-
           externalGrid = ESMF_GridCreate(filename=trim(geomFileName), fileFormat=ESMF_FILEFORMAT_GRIDSPEC, &
             isSphere=.false., addmask=.true., varname=trim(mask_variable), rc=localrc)
           _MOSSCO_LOG_AND_FINALIZE_ON_ERROR_(rc)
