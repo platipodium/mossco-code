fb525028f62448bc4fc2dc003ede86750337b9b7
Knut <knutaros@gmx.net>
2018-07-09 16:51:51 +0200

    getm: add external_index

--- src/components/getm_component.F90
+++ src/components/getm_component.F90
@@ -691,6 +691,9 @@ module getm_component
                                    rc=localrc)
           _LOG_AND_FINALIZE_ON_ERROR_(rc)
 
+          call ESMF_AttributeSet(field,'external_index',conc_id, rc=localrc)
+          _LOG_AND_FINALIZE_ON_ERROR_(rc)
+
           call ESMF_AttributeSet(field,'units','m3 s-1 '//trim(units), rc=localrc)
           _LOG_AND_FINALIZE_ON_ERROR_(rc)
 
@@ -743,6 +746,9 @@ module getm_component
                                    totalUWidth=(/HALO,HALO/),           &
                                    name=trim(itemName),                 &
                                    rc=localrc)
+          _LOG_AND_FINALIZE_ON_ERROR_(rc)
+
+          call ESMF_AttributeSet(field,'external_index',conc_id, rc=localrc)
           _LOG_AND_FINALIZE_ON_ERROR_(rc)
 
           call ESMF_AttributeSet(field,'units','m3 s-1 '//trim(units), rc=localrc)
