7c9c8c470210b6cd551aa5a5573e6ca2c4e3a3fe
Carsten Lemmen <carsten.lemmen@hzg.de>
2018-12-31 15:45:06 +0100

    Create sample mesh in mossco_mesh

--- src/utilities/mossco_mesh.F90
+++ src/utilities/mossco_mesh.F90
@@ -24,7 +24,7 @@ module mossco_mesh
 
   implicit none
 
-  public MOSSCO_MeshString
+  public MOSSCO_MeshString, MOSSCO_MeshCreate
 
   private
 
@@ -73,9 +73,9 @@ subroutine MOSSCO_MeshString(mesh, message, kwe, length, rc)
   _MOSSCO_LOG_AND_FINALIZE_ON_ERROR_(rc_)
 
   write(formatString,'(A,A)') '(A,'//intformat(parametricDim)//',A,',&
-    intformat(spatialDim)//')'    
+    intformat(spatialDim)//')'
   write(string,formatString) '(d=',parametricDim,'/',spatialDim
-  
+
   write(formatString,'(A,A)') '(A,'//intformat(numOwnedNodes)//',A,', &
     intformat(numOwnedElements)//',A)'
   write(string,formatString) trim(string)//' n=',numOwnedNodes,' e=',numOwnedElements,')'
@@ -86,5 +86,85 @@ subroutine MOSSCO_MeshString(mesh, message, kwe, length, rc)
   if (present(rc)) rc=rc_
 
 end subroutine MOSSCO_MeshString
+
+#undef  ESMF_METHOD
+#define ESMF_METHOD "MOSSCO_MeshCreate"
+function MOSSCO_MeshCreate(rc) result(mesh)
+
+  integer(ESMF_KIND_I4), intent(out), optional :: rc
+  type(ESMF_Mesh) :: mesh
+
+  integer(ESMF_KIND_I4) :: numnodes, localrc, rc_
+  integer(ESMF_KIND_I4) :: numQuadElems, numTriElems, numTotElems
+  integer(ESMF_KIND_I4), allocatable :: nodeIds(:), nodeOwners(:), elemIds(:)
+  integer(ESMF_KIND_I4), allocatable :: elemConn(:), elemTypes(:)
+  real(ESMF_KIND_R8), allocatable :: nodeCoords(:)
+
+
+  rc_ = ESMF_SUCCESS
+  if (present(rc)) rc = ESMF_SUCCESS
+
+  mesh = ESMF_MeshCreate(parametricDim=2,spatialDim=2, &
+    coordSys=ESMF_COORDSYS_CART, rc=localrc)
+  _MOSSCO_LOG_AND_FINALIZE_ON_ERROR_(rc_)
+
+  numNodes=9
+  allocate(nodeIds(numNodes))
+  nodeIds=(/1,2,3,4,5,6,7,8,9/)
+
+  ! Since this is a 2D Mesh the size is 2x the number of nodes.
+  allocate(nodeCoords(2*numNodes))
+  nodeCoords=(/0.0,0.0, & ! node id 1
+    1.0,0.0, & ! node id 2
+    2.0,0.0, & ! node id 3
+    0.0,1.0, & ! node id 4
+    1.0,1.0, & ! node id 5
+    2.0,1.0, & ! node id 6
+    0.0,2.0, & ! node id 7
+    1.0,2.0, & ! node id 8
+    2.0,2.0 /) ! node id 9
+
+    ! Since this Mesh is all on PET 0, it’s just set to all 0.
+    allocate(nodeOwners(numNodes))
+    nodeOwners=0 ! everything on PET 0
+
+    call ESMF_MeshAddNodes(mesh, nodeIds=nodeIds, &
+      nodeCoords=nodeCoords, nodeOwners=nodeOwners, rc=localrc)
+    _MOSSCO_LOG_AND_FINALIZE_ON_ERROR_(rc_)
+
+    deallocate(nodeIds)
+    deallocate(nodeCoords)
+    deallocate(nodeOwners)
+
+    numQuadElems=3
+    numTriElems=2
+    numTotElems=numQuadElems+numTriElems
+    allocate(elemIds(numTotElems))
+    elemIds=(/1,2,3,4,5/)
+
+    ! Allocate and fill the element topology type array.
+    allocate(elemTypes(numTotElems))
+    elemTypes=(/ESMF_MESHELEMTYPE_QUAD, & ! elem id 1
+      ESMF_MESHELEMTYPE_TRI, & ! elem id 2
+      ESMF_MESHELEMTYPE_TRI, & ! elem id 3
+      ESMF_MESHELEMTYPE_QUAD, & ! elem id 4
+      ESMF_MESHELEMTYPE_QUAD/) ! elem id 5
+
+    allocate(elemConn(4*numQuadElems+3*numTriElems))
+    elemConn=(/1,2,5,4, & ! elem id 1
+      2,3,5, & ! elem id 2
+      3,6,5, & ! elem id 3
+      4,5,8,7, & ! elem id 4
+      5,6,9,8/) ! elem id 5
+
+    ! Finish the creation of the Mesh by adding the elements
+    call ESMF_MeshAddElements(mesh, elementIds=elemIds,&
+      elementTypes=elemTypes, elementConn=elemConn, rc=localrc)
+
+    deallocate(elemIds)
+    deallocate(elemTypes)
+    deallocate(elemConn)
+
+end function MOSSCO_MeshCreate
 
 end module mossco_mesh
