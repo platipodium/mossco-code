af2701d8cde31966eac64e0047dcaea94836fec7
Carsten Lemmen <carsten.lemmen@hzg.de>
2019-04-10 13:10:21 +0200

    Removed regDecomp from Create in gotm example

--- examples/esmf/gotm/toplevel_component.F90
+++ examples/esmf/gotm/toplevel_component.F90
@@ -39,14 +39,14 @@ module toplevel_component
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
     call ESMF_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_FINALIZE, Finalize, rc=rc)
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-    
+
     rc=ESMF_SUCCESS
     return
 
   end subroutine SetServices
 
   subroutine Initialize(gridComp, importState, exportState, parentClock, rc)
-    
+
     type(ESMF_GridComp)   :: gridComp
     type(ESMF_State)      :: importState
     type(ESMF_State)      :: exportState
@@ -63,8 +63,8 @@ module toplevel_component
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
     ! Create a grid and a field (still to clarify how to access the array
-    grid = ESMF_GridCreateNoPeriDim(minIndex=(/1,1,1/), maxIndex=(/10,20,15/), &
-      regDecomp=(/2,2,1/), name="gotmGrid", rc=rc)
+    grid = ESMF_GridCreateNoPeriDim(minIndex=(/1,1,1/), maxIndex=(/1,1,15/), &
+      name="gotmGrid", rc=rc)
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
     temperatureField = ESMF_FieldCreate(grid,typekind=ESMF_TYPEKIND_R8, &
       indexflag=ESMF_INDEX_DELOCAL, staggerloc=ESMF_STAGGERLOC_CENTER, name = "temperature", rc=rc)
@@ -77,10 +77,10 @@ module toplevel_component
     ! Create component, call setservices, and create states
     gotmComp = ESMF_GridCompCreate(name="gotmComp", grid=grid, rc=rc)
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-   
+
     call ESMF_GridCompSetServices(gotmComp,gotm_SetServices, rc=rc)
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-    
+
     gotmImp = ESMF_StateCreate(stateintent=ESMF_STATEINTENT_IMPORT,name="gotmImp")
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
@@ -95,18 +95,18 @@ module toplevel_component
     call ESMF_GridCompInitialize(gotmComp, importState=gotmImp, exportState=gotmExp, clock=parentClock, rc=rc)
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
-    call ESMF_LogWrite("Toplevel component initialized",ESMF_LOGMSG_INFO) 
+    call ESMF_LogWrite("Toplevel component initialized",ESMF_LOGMSG_INFO)
     rc = ESMF_SUCCESS
 
   end subroutine Initialize
 
   subroutine Run(gridComp, importState, exportState, parentClock, rc)
-    
+
     type(ESMF_GridComp)   :: gridComp
     type(ESMF_State)      :: importState, exportState
     type(ESMF_Clock)      :: parentClock
     integer, intent(out)  :: rc
-  
+
     type(ESMF_Time)       :: clockTime
     character(ESMF_MAXSTR) :: timestring, message
     integer(ESMF_KIND_I8) :: count
@@ -143,8 +143,8 @@ module toplevel_component
 
       call ESMF_ClockAdvance(parentClock, rc=rc)
       if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-      
-    enddo 
+
+    enddo
 
     call ESMF_ClockGet(parentClock,currTime=clockTime, advanceCount=count, rc=rc)
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
@@ -156,11 +156,11 @@ module toplevel_component
     call ESMF_LogWrite(trim(message),ESMF_LOGMSG_INFO)
 
     rc=ESMF_SUCCESS
- 
+
   end subroutine Run
 
   subroutine Finalize(gridComp, importState, exportState, parentClock, rc)
-    
+
     type(ESMF_GridComp)   :: gridComp
     type(ESMF_State)      :: importState, exportState
     type(ESMF_Clock)      :: parentClock
@@ -176,7 +176,7 @@ module toplevel_component
     if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
 
     call ESMF_LogWrite("Toplevel component finalized",ESMF_LOGMSG_INFO)
-   
+
     rc=ESMF_SUCCESS
 
   end subroutine Finalize
