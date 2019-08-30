6047b9a4cec6d1b5f98522864ae994efe0d1aba9
Carsten Lemmen <carsten.lemmen@hzg.de>
2019-08-30 15:52:59 +0200

    Conversion to python3 for stitch_tiles

--- scripts/postprocess/stitch_tiles.py
+++ scripts/postprocess/stitch_tiles.py
@@ -21,9 +21,8 @@ import os
 if len(sys.argv) > 1:
   prefix = sys.argv[1]
 else:
-  prefix = u"mossco_gfs"
-  prefix = u"/Users/lemmen/devel/mossco/setups/sns/netcdf"
-
+  prefix = u"/Users/lemmen/temp/stitch_gfbfrr"
+  #prefix = u"/Users/lemmen/devel/mossco/setups/sns/netcdf"
 
 if prefix.endswith('.nc'):
   pattern=prefix
@@ -32,7 +31,6 @@ if prefix.endswith('.nc'):
     prefix=prefix[0:-1]
 else:
   pattern=prefix + u'.*.nc'
-
 
 petlist=[]
 key=''
@@ -70,12 +68,10 @@ for i in range(1,len(sys.argv)):
     if key=='--level':
         levellist=val.split(',')
 
-#print petlist
 files=glob.glob(pattern)
 if len(petlist) > 0 and len(files) > 0:
     fileparts=files[0].split('.')
     files=[]
-    print fileparts
     petlen=len(fileparts[-2])
     for i,p in enumerate(petlist):
         if petlen==1: f='%s.%1.1d.nc'%(os.path.join(fileparts[:-2])[0],int(p))
@@ -86,11 +82,11 @@ if len(petlist) > 0 and len(files) > 0:
         files.append(f)
 
 if len(files)<1:
-  print "Did not find any files for pattern "+pattern
+  print("Did not find any files for pattern " + pattern)
   quit()
 else:
-  print "Using input files " + pattern + " for output file " + outfile
-
+  print("Using input {0} from {1} files to produce {2}".format(
+    pattern,len(files),outfile))
 
 alat={}
 alon={}
@@ -98,63 +94,56 @@ alon={}
 ## Find coord variables
 
 try:
-  nc=netcdf.Dataset(files[0],'r')
-  print 'Finding variables and dimensions in file ' + files[0]
+    nc=netcdf.Dataset(files[0],'r')
 except:
-  print 'Dataset already open'
+    pass
 
-print 'Found variables ', nc.variables.keys()
-print 'Found dimensions ', nc.dimensions.keys()
 for key, value in nc.variables.items():
-  dim=value.dimensions
-  print key, dim
-  if len(value.dimensions) != 1 : continue
-  if key.endswith('_lat'): alat[key]=[]
-  elif key.endswith('_lon'): alon[key]=[]
-  elif key.endswith('_X'): alon[key]=[]
-  elif key.endswith('_Y'): alat[key]=[]
-  elif key.endswith('_x'): alon[key]=[]
-  elif key.endswith('_y'): alat[key]=[]
+    dim=value.dimensions
+    if len(value.dimensions) != 1 : continue
+    if key.endswith('_lat'): alat[key]=[]
+    elif key.endswith('_lon'): alon[key]=[]
+    elif key.endswith('_X'): alon[key]=[]
+    elif key.endswith('_Y'): alat[key]=[]
+    elif key.endswith('_x'): alon[key]=[]
+    elif key.endswith('_y'): alat[key]=[]
+
 nc.close()
 
-for item in alat.keys():
-  print "Found latitude/Y information " + item
-for item in alon.keys():
-  print "Found longitude/X information " + item
-
 if len(alon)<1:
-  print "Found no longitude/X information"
-  quit()
+    print("Found no longitude/X information")
+    quit()
 if len(alat)<1:
-  print "Found no latitude/Y information"
-  quit()
+    print("Found no latitude/Y information")
+    quit()
+
+print("  horizontal stitch dimensions: "
+    + ", ".join(str(item) for item in alat))
+print("  vertical stitch dimensions:   "
+    + ", ".join(str(item) for item in alon))
 
 for f in files:
-  nc=netcdf.Dataset(f,'r')
+    nc=netcdf.Dataset(f,'r')
 
-  for item in alat.keys():
-    if nc.variables.has_key(item) and alat.has_key(item):
-      alat[item].extend(nc.variables[item][:])
-  for item in alon.keys():
-    if nc.variables.has_key(item) and alon.has_key(item):
-      alon[item].extend(nc.variables[item][:])
-  nc.close()
+    for item in alat:
+        if item in nc.variables:
+            alat[item].extend(nc.variables[item][:])
+    for item in alon:
+        if item in nc.variables:
+            alon[item].extend(nc.variables[item][:])
+    nc.close()
 
+for item in alon:
+    alon[item]=np.unique([v for v in alon[item] if np.isfinite(v)])
+
+for item in alat:
+    alat[item]=np.unique([v for v in alat[item] if np.isfinite(v)])
+
+# Look at the first tile only for time dimension,
+# assume that this is the same for all other tiles
+# Create all other dimensions with information from joined lat/lon above
 nc=netcdf.Dataset(files[0],'r')
 time=nc.variables['time'][:]
-
-for key,value in alon.iteritems(): alon[key]=np.sort(np.unique(value))
-for key,value in alat.iteritems(): alat[key]=np.sort(np.unique(value))
-
-temp={}
-for key,value in alon.iteritems():
-  temp[key]=value[np.isfinite(value)]
-alon=temp
-
-temp={}
-for key,value in alat.iteritems():
-  temp[key]=value[np.isfinite(value)]
-alat=temp
 
 try:
   ncout = netcdf.Dataset(outfile, 'w', format='NETCDF4_CLASSIC')
@@ -162,116 +151,117 @@ except:
   ncout.close()
   ncout = netcdf.Dataset(outfile, 'w', format='NETCDF4_CLASSIC')
 
-for key,value in alon.iteritems():
-  dim=nc.variables[key].dimensions[0]
-  if ncout.dimensions.has_key(dim): continue
-  ncout.createDimension(dim,len(alon[key]))
-for key,value in alat.iteritems():
-  dim=nc.variables[key].dimensions[0]
-  if ncout.dimensions.has_key(dim): continue
-  ncout.createDimension(dim,len(alat[key]))
+# Add stitched dimensions
+for key,value in alon.items():
+    dim=nc.variables[key].dimensions[0]
+    if not dim in ncout.dimensions:
+        ncout.createDimension(dim,len(alon[key]))
+for key,value in alat.items():
+    dim=nc.variables[key].dimensions[0]
+    if not dim in ncout.dimensions:
+        ncout.createDimension(dim,len(alat[key]))
 
-for key,value in nc.dimensions.iteritems():
-  if ncout.dimensions.has_key(key): continue
+# Add non-stitched dimensions
+for key,value in nc.dimensions.items():
+    if not key in ncout.dimensions:
+        ncout.createDimension(key,len(nc.dimensions[key]))
 
-  if key=='time' and len(time)>0: ncout.createDimension('time',len(time))
-  else: ncout.createDimension(key,len(nc.dimensions[key]))
-
-# Create all variables that are in nc also in ncout.  Be careful with _FillValue
-# attribute, as adding this after variable creation causes spurious "variable not
-# found" errors.
+print('  created dimensions ' + ','.join(
+    key + '[' + str(len(value)) + ']'
+    for key,value in ncout.dimensions.items()))
 
 if (incl_variables == []):
-  incl_variables = nc.variables.keys()
+    incl_variables = nc.variables
 
 else:
-  incl_variables.extend(list(set(alon)))
-  incl_variables.extend(list(set(alat)))
+  incl_variables.extend(list(alon))
+  incl_variables.extend(list(alat))
   incl_variables.append('time')
 
-  for key,value in nc.variables.iteritems():
+  for key,value in nc.variables.items():
     try:
         incl_variables.extend(value.coordinates.split(' '))
     except:
         pass
 
 incl_variables=list(set(incl_variables))
-print incl_variables
+print('  include variables ' + ','.join([v for v in incl_variables]))
 
-for key,value in nc.variables.iteritems():
-  dims=list(value.dimensions)
+for key,value in nc.variables.items():
+    dims=value.dimensions
 
-  if (key in incl_variables):
-    try:
-      var=ncout.createVariable(key,value.dtype,tuple(dims),fill_value=value.getncattr('_FillValue'))
-    except:
-      var=ncout.createVariable(key,value.dtype,tuple(dims))
+    if (key in incl_variables):
+        try:
+            var=ncout.createVariable(key,value.dtype,dims,fill_value=value.getncattr('_FillValue'))
+        except:
+            var=ncout.createVariable(key,value.dtype,dims)
 
-    for att in value.ncattrs():
-      if att == '_FillValue': continue
-      var.setncattr(att,value.getncattr(att))
-    print 'Created for output variable ', key , tuple(dims)
+        for att in value.ncattrs():
+            if not att == '_FillValue':
+                var.setncattr(att,value.getncattr(att))
 
-nc.close()
+        print('  created ' + key + '[' + ','.join(dims) + '] ')
+    # + '(' + 'x'.join([len(v) for d,v in nc.dimensions[dims]]) + ')')
 
 # Now add values to time and coordinate variables, close
-if ncout.variables.has_key('time'): ncout.variables['time'][:]=time
+if 'time' in ncout.variables: ncout.variables['time'][:]=time
 
-for item in alat.keys():
-  if ncout.variables.has_key(item):
-    print item, len(ncout.variables[item][:]), len(alat[item])
+for item in alat:
+  if item in ncout.variables:
     ncout.variables[item][:]=alat[item]
   else:
-    print 'Could not find item ' , item, ' in ncout'
-for item in alon.keys():
-  if ncout.variables.has_key(item): ncout.variables[item][:]=alon[item]
+    print('WARNING: Could not find {} in {}'.format(item, outfile))
+for item in alon:
+  if item in ncout.variables:
+    ncout.variables[item][:]=alon[item]
   else:
-    print 'Could not find item ' , item, ' in ncout'
+    print('WARNING: Could not find {} in {}'.format(item, outfile))
 
 for f in files[:]:
 
   try:
     nc=netcdf.Dataset(f,'r')
   except:
-    print 'Dataset is already open'
+    pass
 
   lat={}
   lon={}
   meta={}
 
-  for item in alat.keys():
-    if ncout.variables.has_key(item):
+  for item in alat:
+    if item in ncout.variables:
       temp=nc.variables[item][:]
       if type(temp) is np.ndarray: lat[item] = temp[np.isfinite(temp)]
       elif type(temp) is np.ma.core.MaskedArray: lat[item]=temp.compressed()
       else: lat[item] = temp
-  for item in alon.keys():
-    if ncout.variables.has_key(item):
+  for item in alon:
+    if item in ncout.variables:
       temp=nc.variables[item][:]
       if type(temp) is np.ndarray: lon[item] = temp[np.isfinite(temp)]
       elif type(temp) is np.ma.core.MaskedArray: lon[item]=temp.compressed()
       else:
         lon[item] = temp
 
-  for item in lat.keys():
-    if alat.has_key(item):
+  for item in lat:
+    if item in alat:
       try:
-        if not meta.has_key(item): meta[item]={}
+        #if not meta.has_key(item): meta[item]={}
+        if not item in meta: meta[item]={}
         meta[item]['y']= (np.where(lat[item][0]==alat[item])[0][0], np.where(lat[item][-1]==alat[item])[0][0])
       except:
-        print "Coordinate ", item, ' found not match in ', f
-  for item in lon.keys():
-    if alon.has_key(item):
+        print("WARNING Coordinate ", item, ' found not match in ', f)
+  for item in lon:
+    if item in alon:
       try:
-        if not meta.has_key(item): meta[item]={}
+        if not item in meta: meta[item]={}
         meta[item]['x']= (np.where(lon[item][0]==alon[item])[0][0], np.where(lon[item][-1]==alon[item])[0][0])
       except:
-        print 'Coordinate ', item, ' found no match in ', f
+        print('WARNING Coordinate ', item, ' found no match in ', f)
 
-  coords=alon.keys()
-  coords.extend(alat.keys())
+  coords=list(alon)
+  coords.extend(alat)
 
-  for key,value in nc.variables.iteritems():
+  for key,value in nc.variables.items():
 
     if key in ['time']: continue
     if key in coords: continue
@@ -280,10 +270,10 @@ for f in files[:]:
     var=ncout.variables[key]
 
     if (len(value.shape)) <2:
-      #print 'Skipped variable ', key
+      #print('Skipped variable ', key)
       continue
 
-    print f, key, value.shape
+    print(f, key, value.shape)
 
     dims=list(value.dimensions)
     n=len(dims)
@@ -301,7 +291,7 @@ for f in files[:]:
       # find coordinate variable with axis attribute and same dimension
       for item in coords:
         if ncout.variables[item].dimensions[0]==dims[i]:
-          if alon.has_key(item):
+          if item in alon:
             lbnd[i]=meta[item]['x'][0]
             ubnd[i]=meta[item]['x'][1]+1
 
@@ -318,9 +308,8 @@ for f in files[:]:
             inubnd[i]=len(nc.dimensions[dims[i]])
 
     if np.any(np.array(inubnd)-np.array(inlbnd) != np.array(ubnd) - np.array(lbnd)) :
-      print 'skipped ' + key, lbnd, ubnd, inubnd, inlbnd
+      print('skipped ' + key, lbnd, ubnd, inubnd, inlbnd)
       continue
-
 
     try:
       if n==1:
@@ -334,12 +323,12 @@ for f in files[:]:
         var[lbnd[0]:ubnd[0],lbnd[1]:ubnd[1],lbnd[2]:ubnd[2],lbnd[3]:ubnd[3]] \
         =value[inlbnd[0]:inubnd[0],inlbnd[1]:inubnd[1],inlbnd[2]:inubnd[2],inlbnd[3]:inubnd[3]]
     except:
-      print 'skipped ' + key, lbnd, ubnd, value.shape
+      print('skipped ' + key, lbnd, ubnd, value.shape)
 
       continue
 
     ncout.sync()
-    print 'Stitched ' + f + ' ' + key, value.shape
+    print('Stitched ' + f + ' ' + key, value.shape)
 
   nc.close()
 ncout.close()
