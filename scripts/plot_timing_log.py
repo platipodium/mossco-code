ccf855d99007925550fa16d2d671b4663de9f2c8
Carsten Lemmen <carsten.lemmen@hereon.de>
2024-01-17 17:08:30 +0100

    Make timing log segementation more robust

--- scripts/plot_timing_log.py
+++ scripts/plot_timing_log.py
@@ -12,18 +12,18 @@ import sys
 import os
 import re
 import numpy
-import pylab
+import matplotlib.pyplot as plt
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
-     filename = os.environ['MOSSCO_SETUPDIR'] + '/deep_lake/PET0.deep_lake-1x1-r1'
+    filename = '/Users/Lemmen/temp/PET00.sns_2050-1x61-gf-empty'
+    #filename = os.environ['MOSSCO_SETUPDIR'] + '/deep_lake/PET0.deep_lake-1x1-r1'
 
-print 'Using ' + filename + ' ...'
+print ('Using ' + filename + ' ...')
 
-fid = file(filename,'rU')
-lines=fid.readlines()
-fid.close()
+with open(filename,'r') as fid:
+  lines=fid.readlines()
 
 timingdict={}
 
@@ -32,78 +32,77 @@ for line in lines:
     continue
   if not line.__contains__(' phase '):
     continue
-  words=re.split(' *',line)
+  words=re.split('\s+',line)
   i=words.index('phase')
   pet=words[i-4]
   stage=words[i-1]
   component=words[i-3]
-  if component == 'PET0': 
-    print line
-    continue     
+  if component == 'PET0':
+    print (line)
+    continue
   time=words[1]
-  
+
   msecs=float(time[0:1])*86400000 + float(time[2:3])*3600000 + float(time[4:5])*60000 + float(time[7:9])
-  if not timingdict.has_key(component):
+  if not component in timingdict:
     timingdict[component]={}
-  if not timingdict[component].has_key(stage):
+  if not stage in timingdict[component]:
     timingdict[component][stage]=[]
 
   timingdict[component][stage].append(msecs)
 
 stats={}
-for key,value in timingdict.iteritems():
+for key,value in timingdict.items():
   stats[key]={}
-  if value.has_key('initialized'):
-    timediffs=numpy.array(value['initialized'])-numpy.array(value['initializing'] )
-    timediff=numpy.nansum(timediffs)
-    stats[key]['Initialization']={'all':timediffs, 'total': timediff, 'start': numpy.array(value['initializing'])}      
-  if value.has_key('finalized'):
-    timediffs=numpy.array(value['finalized'])-numpy.array(value['finalizing'] )
-    timediff=numpy.nansum(timediffs)
-    stats[key]['Finalization']={'all':timediffs, 'total': timediff, 'start': numpy.array(value['finalizing'])}
-  if value.has_key('ran'):
-    timediffs=numpy.array(value['ran'])-numpy.array(value['running'] )
-    timediff=numpy.nansum(timediffs)
-    stats[key]['Run']={'all':timediffs, 'total': timediff, 'start': numpy.array(value['running'])}
-
+  try:
+    if 'initialized' in value:
+      timediffs=numpy.array(value['initialized'])-numpy.array(value['initializing'] )
+      timediff=numpy.nansum(timediffs)
+      stats[key]['Initialization']={'all':timediffs, 'total': timediff, 'start': numpy.array(value['initializing'])}
+    if 'finalized' in value:
+      timediffs=numpy.array(value['finalized'])-numpy.array(value['finalizing'] )
+      timediff=numpy.nansum(timediffs)
+      stats[key]['Finalization']={'all':timediffs, 'total': timediff, 'start': numpy.array(value['finalizing'])}
+    if 'ran' in value:
+      timediffs=numpy.array(value['ran'])-numpy.array(value['running'] )
+      timediff=numpy.nansum(timediffs)
+      stats[key]['Run']={'all':timediffs, 'total': timediff, 'start': numpy.array(value['running'])}
+  except:
+      pass
 # make piecharts
-fig=pylab.figure(1, figsize=(6,6))
+fig,ax=plt.subplots(figsize=(6,6))
 fig.clf()
-ax = pylab.axes([0.1, 0.1, 0.8, 0.8])
 labels = timingdict.keys()
-labels.remove('toplevel')
+#labels.pop('toplevel')
 fracs=[]
 for component in labels:
   compsum=0.0
   for stage in ['Initialization', 'Run', 'Finalization']:
-    if stats[component].has_key(stage):
+    if stage in stats[component]:
       compsum=compsum + stats[component][stage]['total']
-  fracs.append(compsum)
-  
-pylab.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-pylab.title('Total time spent by component', bbox={'facecolor':'0.8', 'pad':15})
+  fracs.append(numpy.abs(compsum))
+
+plt.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.title('Total time spent by component', bbox={'facecolor':'0.8', 'pad':15})
 #pylab.show()
-pylab.savefig('total_time_spent_by_component.pdf',transparent=True,format='pdf')
-pylab.close(fig)
+plt.savefig('total_time_spent_by_component.pdf',transparent=True,format='pdf')
+plt.close(fig)
 
 # pycharts separated by phase
 labels = timingdict.keys()
-labels.remove('toplevel')
+#labels.pop('toplevel')
 for stage in ['Initialization', 'Run']: # , 'Finalization']:
-  fig=pylab.figure(figsize=(6,6))
-  fig.clf()
-  ax = pylab.axes([0.1, 0.1, 0.8, 0.8])
+  fig,ax=plt.subplots(figsize=(6,6))
   fracs=[]
   for component in labels:
-    if stats[component].has_key(stage):
-      fracs.append(stats[component][stage]['total'])
+    if stage in stats[component]:
+      fracs.append(numpy.abs(stats[component][stage]['total']))
     else :
       fracs.append(0.0)
-  pylab.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-  pylab.title(stage + ' time spent by component', bbox={'facecolor':'0.8', 'pad':15})
-  #pylab.show()
-  pylab.savefig(stage.lower() + '_time_spent_by_component.pdf',transparent=True,format='pdf')
-  pylab.close(fig)
+  plt.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+  plt.title(stage + ' time spent by component', bbox={'facecolor':'0.8', 'pad':15})
+  plt.show()
+  plt.savefig(stage.lower() + '_time_spent_by_component.pdf',transparent=True,format='pdf')
+  plt.close(fig)
 
 #fig=pylab.figure(5)
 #fig.clf()
@@ -112,7 +111,3 @@ for stage in ['Initialization', 'Run']: # , 'Finalization']:
 #  for stage,stageval in compval.iteritems():
 #    pylab.plot(stageval['start'],stageval['all'])
 #
-
-
-
-
