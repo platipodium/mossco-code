b64dff655ad24c3c7b57e67319e10b3c6ce3232c
Carsten Lemmen <carsten.lemmen@hereon.de>
2025-05-30 08:49:55 +0200

    Added FABM + GOTM cmake modules

---
+++ cmake/FindFABM.cmake
@@ -0,0 +1,38 @@
+# Try to locate FABM's installation prefix.
+# if provided in GOTM_PREFIX, use FABM modules used by GOTM
+find_path(FABM_PREFIX_PATH
+  NAMES include/fabm_driver.h
+  HINTS "${FABM_PREFIX}" "${GOTM_PREFIX}" "$ENV{FABM_PREFIX}"
+  PATHS "$ENV{LOCALAPPDATA}/fabm/gotm" "$ENV{APPDATA}/fabm/gotm" "$ENV{HOME}/local/fabm/gotm"
+  DOC "Installation prefix for Framework for Aquatic Biogeochemical Models - fabm.net"
+)
+
+# Find FABM library
+find_library(FABM_LIBRARIES NAMES fabm
+             HINTS ${FABM_PREFIX_PATH}/lib
+             DOC "FABM libraries")
+
+# Store configurable path of FABM include directory
+find_path(FABM_INCLUDE_DIRS
+          NAMES fabm_driver.h
+          HINTS ${FABM_PREFIX_PATH}/include
+          DOC "FABM include directory"
+)
+
+mark_as_advanced(FABM_LIBRARIES FABM_INCLUDE_DIRS)
+
+if(NOT EXISTS ${FABM_INCLUDE_DIRS}/fabm_v0_compatibility.mod)
+add_definitions(-D_OLD_FABM_)
+endif()
+if(NOT EXISTS ${FABM_INCLUDE_DIRS}/fabm_version.h)
+add_definitions(-D_FABM_API_VERSION_=0)
+endif()
+
+
+# Process default arguments (QUIET, REQUIRED)
+include(FindPackageHandleStandardArgs) 
+find_package_handle_standard_args (FABM DEFAULT_MSG FABM_LIBRARIES FABM_INCLUDE_DIRS) 
+
+# For backward compatibility:
+set(FABM_LIBRARY FABM_LIBRARIES)
+set(FABM_INCLUDE_DIR FABM_INCLUDE_DIRS)
---
+++ cmake/FindGOTM.cmake
@@ -0,0 +1,64 @@
+# Try to locate GOTM's installation prefix.
+find_path(GOTM_PREFIX
+  NAMES include/turbulence.mod
+  HINTS "$ENV{GOTM_PREFIX}"
+  PATHS "$ENV{LOCALAPPDATA}/gotm" "$ENV{APPDATA}/gotm" "$ENV{HOME}/local/gotm"
+  DOC "Installation prefix for General Ocean Turbulence Models - gotm.net"
+)
+
+# Find GOTM/FABM coupling library if USE_FABM
+if(GETM_USE_FABM)
+  find_library(GOTM_FABM NAMES gotm_fabm
+               HINTS ${GOTM_PREFIX}/lib
+               DOC "GOTM-FABM library")
+endif(GETM_USE_FABM)
+
+# Find GOTM turbulence library
+find_library(GOTM_TURBULENCE NAMES turbulence
+             HINTS ${GOTM_PREFIX}/lib
+             DOC "GOTM turbulence library")
+
+# Find GOTM output_manager library
+find_library(GOTM_OUTMAN NAMES output_manager
+             HINTS ${GOTM_PREFIX}/lib
+             DOC "GOTM output_manager library")
+
+# Find GOTM utility library
+find_library(GOTM_UTIL NAMES util
+             HINTS ${GOTM_PREFIX}/lib
+             DOC "GOTM utility library")
+
+# Find GOTM field_manager library
+find_library(GOTM_FIELDMAN NAMES field_manager
+             HINTS ${GOTM_PREFIX}/lib
+             DOC "GOTM field_manager library")
+
+# Find GOTM yaml library
+find_library(GOTM_YAML NAMES yaml
+             HINTS ${GOTM_PREFIX}/lib
+             DOC "GOTM yaml library")
+
+set(GOTM_LIBRARIES ${GOTM_FABM} ${GOTM_TURBULENCE} ${GOTM_OUTMAN} ${GOTM_UTIL})
+
+# libfield_manager and libyaml only exist in the new GOTM
+if(GOTM_FIELDMAN)
+add_definitions(-D_NEW_GOTM_)
+set(GOTM_LIBRARIES ${GOTM_LIBRARIES} ${GOTM_FIELDMAN})
+endif()
+if(GOTM_YAML)
+set(GOTM_LIBRARIES ${GOTM_LIBRARIES} ${GOTM_YAML})
+endif()
+
+
+# Store configurable path of GOTM include directory
+find_path(GOTM_INCLUDE_DIRS
+          NAMES turbulence.mod
+          HINTS ${GOTM_PREFIX}/include
+          DOC "GOTM include directories"
+)
+
+mark_as_advanced(GOTM_LIBRARIES GOTM_INCLUDE_DIRS GOTM_TURBULENCE GOTM_OUTMAN GOTM_FIELDMAN GOTM_YAML GOTM_UTIL GOTM_FABM)
+
+# Process default arguments (QUIET, REQUIRED)
+include(FindPackageHandleStandardArgs) 
+find_package_handle_standard_args (GOTM DEFAULT_MSG GOTM_LIBRARIES GOTM_INCLUDE_DIRS) 
